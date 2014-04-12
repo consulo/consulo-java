c0043c1eb19560fb1b498e43c311d825a1bb931b
VISTALL <vistall.valeriy@gmail.com>
2014-04-12 20:04:08 +0300

    dont use reflect - use jdi

--- java-debugger-impl/src/com/intellij/debugger/jdi/LocalVariablesUtil.java
+++ java-debugger-impl/src/com/intellij/debugger/jdi/LocalVariablesUtil.java
@@ -15,185 +15,56 @@
  */
 package com.intellij.debugger.jdi;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.intellij.openapi.diagnostic.Logger;
 import consulo.internal.com.sun.jdi.InternalException;
 import consulo.internal.com.sun.jdi.StackFrame;
 import consulo.internal.com.sun.jdi.Value;
 import consulo.internal.com.sun.jdi.VirtualMachine;
+import consulo.internal.com.sun.tools.jdi.JDWP;
+import consulo.internal.com.sun.tools.jdi.StackFrameImpl;
+import consulo.internal.com.sun.tools.jdi.ThreadReferenceImpl;
+import consulo.internal.com.sun.tools.jdi.VirtualMachineImpl;
 
-/**
- * From JDI sources:
- * <p/>
- * validateStackFrame();
- * validateMirrors(variables);
- * <p/>
- * int count = variables.size();
- * JDWP.StackFrame.GetValues.SlotInfo[] slots =
- * new JDWP.StackFrame.GetValues.SlotInfo[count];
- * <p/>
- * for (int i=0; i<count; ++i) {
- * LocalVariableImpl variable = (LocalVariableImpl)variables.get(i);
- * if (!variable.isVisible(this)) {
- * throw new IllegalArgumentException(variable.name() +
- * " is not valid at this frame location");
- * }
- * slots[i] = new JDWP.StackFrame.GetValues.SlotInfo(variable.slot(),
- * (byte)variable.signature().charAt(0));
- * }
- * <p/>
- * PacketStream ps;
- * <p/>
- * synchronized (vm.state()) {
- * validateStackFrame();
- * ps = JDWP.StackFrame.GetValues.enqueueCommand(vm, thread, id, slots);
- * }
- * <p/>
- * ValueImpl[] values;
- * try {
- * values = JDWP.StackFrame.GetValues.waitForReply(vm, ps).values;
- * } catch (JDWPException exc) {
- * switch (exc.errorCode()) {
- * case JDWP.Error.INVALID_FRAMEID:
- * case JDWP.Error.THREAD_NOT_SUSPENDED:
- * case JDWP.Error.INVALID_THREAD:
- * throw new InvalidStackFrameException();
- * default:
- * throw exc.toJDIException();
- * }
- * }
- * <p/>
- * if (count != values.length) {
- * throw new InternalException(
- * "Wrong number of values returned from target VM");
- * }
- * Map map = new HashMap(count);
- * for (int i=0; i<count; ++i) {
- * LocalVariableImpl variable = (LocalVariableImpl)variables.get(i);
- * map.put(variable, values[i]);
- * }
- * return map;
- */
 public class LocalVariablesUtil
 {
-	private static final Logger LOG = Logger.getInstance("#com.intellij.debugger.jdi.LocalVariablesUtil");
-
-	private static final boolean ourInitializationOk;
-	private static Class<?> ourSlotInfoClass;
-	private static Constructor<?> slotInfoConstructor;
-	private static Class<?> ourGetValuesClass;
-	private static Method ourEnqueueMethod;
-	private static Method ourWaitForReplyMethod;
-
-	static
-	{
-		boolean success = false;
-		try
-		{
-			ourSlotInfoClass = Class.forName("consulo.internal.com.sun.tools.jdi.JDWP$StackFrame$GetValues$SlotInfo");
-			slotInfoConstructor = ourSlotInfoClass.getDeclaredConstructor(int.class, byte.class);
-			slotInfoConstructor.setAccessible(true);
-
-			ourGetValuesClass = Class.forName("consulo.internal.com.sun.tools.jdi.JDWP$StackFrame$GetValues");
-			ourEnqueueMethod = findMethod(ourGetValuesClass, "enqueueCommand");
-			ourEnqueueMethod.setAccessible(true);
-			ourWaitForReplyMethod = findMethod(ourGetValuesClass, "waitForReply");
-			ourWaitForReplyMethod.setAccessible(true);
-
-			success = true;
-		}
-		catch(Throwable e)
-		{
-			LOG.info(e);
-		}
-		ourInitializationOk = success;
-	}
-
 	public static Map<DecompiledLocalVariable, Value> fetchValues(StackFrame frame, Collection<DecompiledLocalVariable> vars) throws Exception
 	{
-		if(!ourInitializationOk)
-		{
-			return Collections.emptyMap();
-		}
-		final Field frameIdField = frame.getClass().getDeclaredField("id");
-		frameIdField.setAccessible(true);
-		final Object frameId = frameIdField.get(frame);
+		final long frameId = ((StackFrameImpl)frame).id();
 
 		final VirtualMachine vm = frame.virtualMachine();
-		final Method stateMethod = vm.getClass().getDeclaredMethod("state");
-		stateMethod.setAccessible(true);
 
-		Object slotInfoArray = createSlotInfoArray(vars);
+		JDWP.StackFrame.GetValues.SlotInfo[] slotInfoArray = createSlotInfoArray(vars);
 
-		Object ps;
-		final Object vmState = stateMethod.invoke(vm);
-		synchronized(vmState)
-		{
-			ps = ourEnqueueMethod.invoke(null, vm, frame.thread(), frameId, slotInfoArray);
-		}
+		JDWP.StackFrame.GetValues process = JDWP.StackFrame.GetValues.process((VirtualMachineImpl) vm, (ThreadReferenceImpl)frame.thread(), frameId,
+				slotInfoArray);
 
-		try
+		final Value[] values = process.values;
+		if(vars.size() != values.length)
 		{
-			final Object reply = ourWaitForReplyMethod.invoke(null, vm, ps);
-			final Field valuesField = reply.getClass().getDeclaredField("values");
-			valuesField.setAccessible(true);
-			final Value[] values = (Value[]) valuesField.get(reply);
-			if(vars.size() != values.length)
-			{
-				throw new InternalException("Wrong number of values returned from target VM");
-			}
-			final Map<DecompiledLocalVariable, Value> map = new HashMap<DecompiledLocalVariable, Value>(vars.size());
-			int idx = 0;
-			for(DecompiledLocalVariable var : vars)
-			{
-				map.put(var, values[idx++]);
-			}
-			return map;
+			throw new InternalException("Wrong number of values returned from target VM");
 		}
-		catch(InvocationTargetException e)
+		final Map<DecompiledLocalVariable, Value> map = new HashMap<DecompiledLocalVariable, Value>(vars.size());
+		int idx = 0;
+		for(DecompiledLocalVariable var : vars)
 		{
-			final Throwable target = e.getTargetException();
-			if(target instanceof Exception)
-			{
-				throw (Exception) target;
-			}
-			throw e;
+			map.put(var, values[idx++]);
 		}
+		return map;
 	}
 
-	private static Object createSlotInfoArray(Collection<DecompiledLocalVariable> vars) throws Exception
+	private static JDWP.StackFrame.GetValues.SlotInfo[] createSlotInfoArray(Collection<DecompiledLocalVariable> vars) throws Exception
 	{
-		final Object arrayInstance = Array.newInstance(ourSlotInfoClass, vars.size());
+		final JDWP.StackFrame.GetValues.SlotInfo[]  arrayInstance = new JDWP.StackFrame.GetValues.SlotInfo[vars.size()];
 
 		int idx = 0;
 		for(DecompiledLocalVariable var : vars)
 		{
-			final Object info = slotInfoConstructor.newInstance(var.getSlot(), (byte) var.getSignature().charAt(0));
-			Array.set(arrayInstance, idx++, info);
+			arrayInstance[idx] = new JDWP.StackFrame.GetValues.SlotInfo(var.getSlot(), (byte) var.getSignature().charAt(0));
 		}
 
 		return arrayInstance;
 	}
-
-	private static Method findMethod(Class aClass, String methodName) throws NoSuchMethodException
-	{
-		for(Method method : aClass.getDeclaredMethods())
-		{
-			if(methodName.equals(method.getName()))
-			{
-				return method;
-			}
-		}
-		throw new NoSuchMethodException(aClass.getName() + "." + methodName);
-	}
-
 }
