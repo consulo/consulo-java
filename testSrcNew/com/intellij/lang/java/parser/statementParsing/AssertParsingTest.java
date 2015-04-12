34270d6e33c89f48275ff64f2fdf6e808291540b
VISTALL <vistall.valeriy@gmail.com>
2015-04-12 20:22:39 +0300

    preparing for tests

--- testSrcNew/com/intellij/lang/java/parser/statementParsing/AssertParsingTest.java
+++ testSrcNew/com/intellij/lang/java/parser/statementParsing/AssertParsingTest.java
@@ -17,11 +17,20 @@ package com.intellij.lang.java.parser.statementParsing;
 
 import com.intellij.lang.java.parser.JavaParsingTestCase;
 
-public class AssertParsingTest extends JavaParsingTestCase {
-  public AssertParsingTest() {
-    super("parser-full/statementParsing/assert");
-  }
+public class AssertParsingTest extends JavaParsingTestCase
+{
+	public AssertParsingTest()
+	{
+		super("parser-full/statementParsing/assert");
+	}
 
-  public void testNormal1() { doTest(true); }
-  public void testNormal2() { doTest(true); }
+	public void testNormal1()
+	{
+		doTest(true);
+	}
+
+	public void testNormal2()
+	{
+		doTest(true);
+	}
 }
