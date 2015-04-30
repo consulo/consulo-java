fb6120baad6f8b8703152e129212c49dd1706605
VISTALL <vistall.valeriy@gmail.com>
2015-04-30 18:52:30 +0300

    reformat

--- java-impl/src/com/intellij/lang/java/JavaRefactoringSupportProvider.java
+++ java-impl/src/com/intellij/lang/java/JavaRefactoringSupportProvider.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.lang.java;
 
+import org.jetbrains.annotations.Nullable;
 import com.intellij.lang.refactoring.RefactoringSupportProvider;
 import com.intellij.psi.*;
 import com.intellij.psi.javadoc.PsiDocComment;
@@ -35,129 +36,169 @@ import com.intellij.refactoring.introduceParameter.IntroduceParameterHandler;
 import com.intellij.refactoring.introduceVariable.IntroduceVariableHandler;
 import com.intellij.refactoring.memberPullUp.JavaPullUpHandler;
 import com.intellij.refactoring.memberPushDown.JavaPushDownHandler;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author ven
  */
-public class JavaRefactoringSupportProvider extends RefactoringSupportProvider {
-  @Override
-  public boolean isSafeDeleteAvailable(PsiElement element) {
-    return element instanceof PsiClass || element instanceof PsiMethod || element instanceof PsiField ||
-           (element instanceof PsiParameter && ((PsiParameter)element).getDeclarationScope() instanceof PsiMethod) ||
-           element instanceof PsiJavaPackage || element instanceof PsiLocalVariable;
-  }
+public class JavaRefactoringSupportProvider extends RefactoringSupportProvider
+{
+	@Override
+	public boolean isSafeDeleteAvailable(PsiElement element)
+	{
+		return element instanceof PsiClass || element instanceof PsiMethod || element instanceof PsiField ||
+				(element instanceof PsiParameter && ((PsiParameter) element).getDeclarationScope() instanceof PsiMethod) ||
+				element instanceof PsiJavaPackage || element instanceof PsiLocalVariable;
+	}
 
-  @Override
-  public RefactoringActionHandler getIntroduceConstantHandler() {
-    return new IntroduceConstantHandler();
-  }
+	@Override
+	public RefactoringActionHandler getIntroduceConstantHandler()
+	{
+		return new IntroduceConstantHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getIntroduceFieldHandler() {
-    return new IntroduceFieldHandler();
-  }
+	@Override
+	public RefactoringActionHandler getIntroduceFieldHandler()
+	{
+		return new IntroduceFieldHandler();
+	}
 
-  @Override
-  public boolean isInplaceRenameAvailable(final PsiElement element, final PsiElement context) {
-    return mayRenameInplace(element, context);
-  }
+	@Override
+	public boolean isInplaceRenameAvailable(final PsiElement element, final PsiElement context)
+	{
+		return mayRenameInplace(element, context);
+	}
 
-  @Override
-  public boolean isMemberInplaceRenameAvailable(PsiElement elementToRename, PsiElement context) {
-    return elementToRename instanceof PsiMember;
-  }
+	@Override
+	public boolean isMemberInplaceRenameAvailable(PsiElement elementToRename, PsiElement context)
+	{
+		return elementToRename instanceof PsiMember;
+	}
 
-  @Override
-  public RefactoringActionHandler getIntroduceVariableHandler() {
-    return new IntroduceVariableHandler();
-  }
+	@Override
+	public RefactoringActionHandler getIntroduceVariableHandler()
+	{
+		return new IntroduceVariableHandler();
+	}
 
-  @Override
-  @Nullable
-  public RefactoringActionHandler getExtractMethodHandler() {
-    return new ExtractMethodHandler();
-  }
+	@Override
+	@Nullable
+	public RefactoringActionHandler getExtractMethodHandler()
+	{
+		return new ExtractMethodHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getIntroduceParameterHandler() {
-    return new IntroduceParameterHandler();
-  }
+	@Override
+	public RefactoringActionHandler getIntroduceParameterHandler()
+	{
+		return new IntroduceParameterHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getPullUpHandler() {
-    return new JavaPullUpHandler();
-  }
+	@Override
+	public RefactoringActionHandler getPullUpHandler()
+	{
+		return new JavaPullUpHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getPushDownHandler() {
-    return new JavaPushDownHandler();
-  }
+	@Override
+	public RefactoringActionHandler getPushDownHandler()
+	{
+		return new JavaPushDownHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getExtractInterfaceHandler() {
-    return new ExtractInterfaceHandler();
-  }
+	@Override
+	public RefactoringActionHandler getExtractInterfaceHandler()
+	{
+		return new ExtractInterfaceHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getExtractSuperClassHandler() {
-    return new ExtractSuperclassHandler();
-  }
+	@Override
+	public RefactoringActionHandler getExtractSuperClassHandler()
+	{
+		return new ExtractSuperclassHandler();
+	}
 
-  @Override
-  public ChangeSignatureHandler getChangeSignatureHandler() {
-    return new JavaChangeSignatureHandler();
-  }
+	@Override
+	public ChangeSignatureHandler getChangeSignatureHandler()
+	{
+		return new JavaChangeSignatureHandler();
+	}
 
-  @Override
-  public RefactoringActionHandler getExtractClassHandler() {
-    return new ExtractClassHandler();
-  }
+	@Override
+	public RefactoringActionHandler getExtractClassHandler()
+	{
+		return new ExtractClassHandler();
+	}
 
-  @Override
-  public boolean isInplaceIntroduceAvailable(PsiElement element, PsiElement context) {
-    if (!(element instanceof PsiExpression)) return false;
-    if (context == null || context.getContainingFile() != element.getContainingFile()) return false;
-    return true;
-  }
+	@Override
+	public boolean isInplaceIntroduceAvailable(PsiElement element, PsiElement context)
+	{
+		if(!(element instanceof PsiExpression))
+		{
+			return false;
+		}
+		if(context == null || context.getContainingFile() != element.getContainingFile())
+		{
+			return false;
+		}
+		return true;
+	}
 
-  public static boolean mayRenameInplace(PsiElement elementToRename, final PsiElement nameSuggestionContext) {
-    if (nameSuggestionContext != null && nameSuggestionContext.getContainingFile() != elementToRename.getContainingFile()) return false;
-    if (!(elementToRename instanceof PsiLocalVariable) &&
-        !(elementToRename instanceof PsiParameter) &&
-        !(elementToRename instanceof PsiLabeledStatement)) {
-      return false;
-    }
-    SearchScope useScope = PsiSearchHelper.SERVICE.getInstance(elementToRename.getProject()).getUseScope(elementToRename);
-    if (!(useScope instanceof LocalSearchScope)) return false;
-    PsiElement[] scopeElements = ((LocalSearchScope)useScope).getScope();
-    if (scopeElements.length > 1 &&                          // assume there are no elements with use scopes with holes in them
-        !isElementWithComment(scopeElements) &&              // ... except a case of element and it's doc comment
-        !isResourceVariable(scopeElements)) {
-      return false;    // ... and badly scoped resource variables
-    }
-    PsiFile containingFile = elementToRename.getContainingFile();
-    return PsiTreeUtil.isAncestor(containingFile, scopeElements[0], false);
-  }
+	public static boolean mayRenameInplace(PsiElement elementToRename, final PsiElement nameSuggestionContext)
+	{
+		if(nameSuggestionContext != null && nameSuggestionContext.getContainingFile() != elementToRename.getContainingFile())
+		{
+			return false;
+		}
+		if(!(elementToRename instanceof PsiLocalVariable) &&
+				!(elementToRename instanceof PsiParameter) &&
+				!(elementToRename instanceof PsiLabeledStatement))
+		{
+			return false;
+		}
+		SearchScope useScope = PsiSearchHelper.SERVICE.getInstance(elementToRename.getProject()).getUseScope(elementToRename);
+		if(!(useScope instanceof LocalSearchScope))
+		{
+			return false;
+		}
+		PsiElement[] scopeElements = ((LocalSearchScope) useScope).getScope();
+		if(scopeElements.length > 1 &&                          // assume there are no elements with use scopes with holes in them
+				!isElementWithComment(scopeElements) &&              // ... except a case of element and it's doc comment
+				!isResourceVariable(scopeElements))
+		{
+			return false;    // ... and badly scoped resource variables
+		}
+		PsiFile containingFile = elementToRename.getContainingFile();
+		return PsiTreeUtil.isAncestor(containingFile, scopeElements[0], false);
+	}
 
-  private static boolean isElementWithComment(final PsiElement[] scopeElements) {
-    if (scopeElements.length > 2) return false;
+	private static boolean isElementWithComment(final PsiElement[] scopeElements)
+	{
+		if(scopeElements.length > 2)
+		{
+			return false;
+		}
 
-    PsiDocComment comment = null;
-    PsiDocCommentOwner owner = null;
-    for (PsiElement element : scopeElements) {
-      if (element instanceof PsiDocComment) {
-        comment = (PsiDocComment)element;
-      }
-      else if (element instanceof PsiDocCommentOwner) owner = (PsiDocCommentOwner)element;
-    }
+		PsiDocComment comment = null;
+		PsiDocCommentOwner owner = null;
+		for(PsiElement element : scopeElements)
+		{
+			if(element instanceof PsiDocComment)
+			{
+				comment = (PsiDocComment) element;
+			}
+			else if(element instanceof PsiDocCommentOwner)
+			{
+				owner = (PsiDocCommentOwner) element;
+			}
+		}
 
-    return comment != null && comment.getOwner() == owner;
-  }
+		return comment != null && comment.getOwner() == owner;
+	}
 
-  private static boolean isResourceVariable(final PsiElement[] scopeElements) {
-    return scopeElements.length == 2 &&
-           scopeElements[0] instanceof PsiResourceList &&
-           scopeElements[1] instanceof PsiCodeBlock;
-  }
+	private static boolean isResourceVariable(final PsiElement[] scopeElements)
+	{
+		return scopeElements.length == 2 &&
+				scopeElements[0] instanceof PsiResourceList &&
+				scopeElements[1] instanceof PsiCodeBlock;
+	}
 }
