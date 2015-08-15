6bb4caae4fef6006c391e5ab7ad019125d8b9a49
VISTALL <vistall.valeriy@gmail.com>
2015-08-15 14:09:30 +0300

    reformat

--- java-indexing-api/src/com/intellij/psi/search/searches/MethodReferencesSearch.java
+++ java-indexing-api/src/com/intellij/psi/search/searches/MethodReferencesSearch.java
@@ -15,112 +15,146 @@
  */
 package com.intellij.psi.search.searches;
 
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.search.*;
-import com.intellij.util.*;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.QuerySearchRequest;
+import com.intellij.psi.search.SearchRequestCollector;
+import com.intellij.psi.search.SearchRequestQuery;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.SearchSession;
+import com.intellij.util.MergeQuery;
+import com.intellij.util.PairProcessor;
+import com.intellij.util.Processor;
+import com.intellij.util.Query;
+import com.intellij.util.UniqueResultsQuery;
+import com.intellij.util.containers.ContainerUtil;
 
 /**
  * @author max
  */
-public class MethodReferencesSearch extends ExtensibleQueryFactory<PsiReference, MethodReferencesSearch.SearchParameters> {
-  public static final MethodReferencesSearch INSTANCE = new MethodReferencesSearch();
+public class MethodReferencesSearch extends ExtensibleQueryFactory<PsiReference,
+		MethodReferencesSearch.SearchParameters>
+{
+	public static final MethodReferencesSearch INSTANCE = new MethodReferencesSearch();
 
-  public static class SearchParameters {
-    private final PsiMethod myMethod;
-    private final SearchScope myScope;
-    private final boolean myStrictSignatureSearch;
-    private final SearchRequestCollector myOptimizer;
-    private final boolean isSharedOptimizer;
+	public static class SearchParameters
+	{
+		private final PsiMethod myMethod;
+		private final SearchScope myScope;
+		private final boolean myStrictSignatureSearch;
+		private final SearchRequestCollector myOptimizer;
+		private final boolean isSharedOptimizer;
 
-    public SearchParameters(PsiMethod method,
-                            SearchScope scope,
-                            boolean strictSignatureSearch,
-                            @Nullable SearchRequestCollector optimizer) {
-      myMethod = method;
-      myScope = scope;
-      myStrictSignatureSearch = strictSignatureSearch;
-      isSharedOptimizer = optimizer != null;
-      myOptimizer = optimizer != null ? optimizer : new SearchRequestCollector(new SearchSession());
-    }
+		public SearchParameters(PsiMethod method,
+				SearchScope scope,
+				boolean strictSignatureSearch,
+				@Nullable SearchRequestCollector optimizer)
+		{
+			myMethod = method;
+			myScope = scope;
+			myStrictSignatureSearch = strictSignatureSearch;
+			isSharedOptimizer = optimizer != null;
+			myOptimizer = optimizer != null ? optimizer : new SearchRequestCollector(new SearchSession());
+		}
 
-    public SearchParameters(final PsiMethod aClass, SearchScope scope, final boolean strict) {
-      this(aClass, scope, strict, null);
-    }
+		public SearchParameters(final PsiMethod aClass, SearchScope scope, final boolean strict)
+		{
+			this(aClass, scope, strict, null);
+		}
 
-    public PsiMethod getMethod() {
-      return myMethod;
-    }
+		public PsiMethod getMethod()
+		{
+			return myMethod;
+		}
 
-    public boolean isStrictSignatureSearch() {
-      return myStrictSignatureSearch;
-    }
+		public boolean isStrictSignatureSearch()
+		{
+			return myStrictSignatureSearch;
+		}
 
-    public SearchRequestCollector getOptimizer() {
-      return myOptimizer;
-    }
+		public SearchRequestCollector getOptimizer()
+		{
+			return myOptimizer;
+		}
 
-    public SearchScope getScope() {
-      return myScope;
-    }
-  }
+		public SearchScope getScope()
+		{
+			return myScope;
+		}
+	}
 
-  private MethodReferencesSearch() {
-    super("org.consulo.java");
-  }
+	private MethodReferencesSearch()
+	{
+		super("org.consulo.java");
+	}
 
-  public static Query<PsiReference> search(final PsiMethod method, SearchScope scope, final boolean strictSignatureSearch) {
-    return search(new SearchParameters(method, scope, strictSignatureSearch));
-  }
+	public static Query<PsiReference> search(final PsiMethod method,
+			SearchScope scope,
+			final boolean strictSignatureSearch)
+	{
+		return search(new SearchParameters(method, scope, strictSignatureSearch));
+	}
 
-  public static void searchOptimized(final PsiMethod method,
-                                     SearchScope scope,
-                                     final boolean strictSignatureSearch,
-                                     @NotNull SearchRequestCollector collector,
-                                     final Processor<PsiReference> processor) {
-    searchOptimized(method, scope, strictSignatureSearch, collector, false, new PairProcessor<PsiReference, SearchRequestCollector>() {
-      @Override
-      public boolean process(PsiReference psiReference, SearchRequestCollector collector) {
-        return processor.process(psiReference);
-      }
-    });
-  }
+	public static void searchOptimized(final PsiMethod method,
+			SearchScope scope,
+			final boolean strictSignatureSearch,
+			@NotNull SearchRequestCollector collector,
+			final Processor<PsiReference> processor)
+	{
+		searchOptimized(method, scope, strictSignatureSearch, collector, false, new PairProcessor<PsiReference,
+				SearchRequestCollector>()
+		{
+			@Override
+			public boolean process(PsiReference psiReference, SearchRequestCollector collector)
+			{
+				return processor.process(psiReference);
+			}
+		});
+	}
 
-  public static void searchOptimized(final PsiMethod method,
-                                     SearchScope scope,
-                                     final boolean strictSignatureSearch,
-                                     SearchRequestCollector collector,
-                                     final boolean inReadAction,
-                                     PairProcessor<PsiReference, SearchRequestCollector> processor) {
-    final SearchRequestCollector nested = new SearchRequestCollector(collector.getSearchSession());
-    collector.searchQuery(
-      new QuerySearchRequest(search(new SearchParameters(method, scope, strictSignatureSearch, nested)), nested, inReadAction, processor));
-  }
+	public static void searchOptimized(final PsiMethod method,
+			SearchScope scope,
+			final boolean strictSignatureSearch,
+			SearchRequestCollector collector,
+			final boolean inReadAction,
+			PairProcessor<PsiReference, SearchRequestCollector> processor)
+	{
+		final SearchRequestCollector nested = new SearchRequestCollector(collector.getSearchSession());
+		collector.searchQuery(new QuerySearchRequest(search(new SearchParameters(method, scope, strictSignatureSearch,
+				nested)), nested, inReadAction, processor));
+	}
 
-  public static Query<PsiReference> search(final SearchParameters parameters) {
-    final Query<PsiReference> result = INSTANCE.createQuery(parameters);
-    if (parameters.isSharedOptimizer) {
-      return uniqueResults(result);
-    }
+	public static Query<PsiReference> search(final SearchParameters parameters)
+	{
+		final Query<PsiReference> result = INSTANCE.createQuery(parameters);
+		if(parameters.isSharedOptimizer)
+		{
+			return uniqueResults(result);
+		}
 
-    final SearchRequestCollector requests = parameters.getOptimizer();
+		final SearchRequestCollector requests = parameters.getOptimizer();
 
-    return uniqueResults(new MergeQuery<PsiReference>(result, new SearchRequestQuery(parameters.getMethod().getProject(), requests)));
-  }
+		return uniqueResults(new MergeQuery<PsiReference>(result, new SearchRequestQuery(parameters.getMethod()
+				.getProject(), requests)));
+	}
 
-  public static Query<PsiReference> search(final PsiMethod method, final boolean strictSignatureSearch) {
-    return search(method, GlobalSearchScope.allScope(method.getProject()), strictSignatureSearch);
-  }
+	public static Query<PsiReference> search(final PsiMethod method, final boolean strictSignatureSearch)
+	{
+		return search(method, GlobalSearchScope.allScope(method.getProject()), strictSignatureSearch);
+	}
 
-  public static Query<PsiReference> search(final PsiMethod method) {
-    return search(method, true);
-  }
+	public static Query<PsiReference> search(final PsiMethod method)
+	{
+		return search(method, true);
+	}
 
-  private static UniqueResultsQuery<PsiReference, ReferenceDescriptor> uniqueResults(@NotNull Query<PsiReference> composite) {
-    return new UniqueResultsQuery<PsiReference, ReferenceDescriptor>(composite, ContainerUtil.<ReferenceDescriptor>canonicalStrategy(),
-                                                                     ReferenceDescriptor.MAPPER);
-  }
+	private static UniqueResultsQuery<PsiReference, ReferenceDescriptor> uniqueResults(@NotNull Query<PsiReference>
+			composite)
+	{
+		return new UniqueResultsQuery<PsiReference, ReferenceDescriptor>(composite,
+				ContainerUtil.<ReferenceDescriptor>canonicalStrategy(), ReferenceDescriptor.MAPPER);
+	}
 }
