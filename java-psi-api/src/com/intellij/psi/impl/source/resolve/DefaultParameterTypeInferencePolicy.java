8addfdaef12f3b9ff2b27cce61bbe51eaf9bc0ce
VISTALL <vistall.valeriy@gmail.com>
2015-08-22 16:00:42 +0300

    formatting

--- java-psi-api/src/com/intellij/psi/impl/source/resolve/DefaultParameterTypeInferencePolicy.java
+++ java-psi-api/src/com/intellij/psi/impl/source/resolve/DefaultParameterTypeInferencePolicy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,37 +15,48 @@
  */
 package com.intellij.psi.impl.source.resolve;
 
-import com.intellij.openapi.util.Pair;
-import com.intellij.psi.*;
 import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.ConstraintType;
+import com.intellij.psi.PsiCallExpression;
+import com.intellij.psi.PsiExpression;
+import com.intellij.psi.PsiExpressionList;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiTypeParameter;
 
 /**
  * @author yole
  */
-public class DefaultParameterTypeInferencePolicy extends ParameterTypeInferencePolicy {
-  public static final DefaultParameterTypeInferencePolicy INSTANCE = new DefaultParameterTypeInferencePolicy();
-  
-  @Nullable
-  @Override
-  public Pair<PsiType, ConstraintType> inferTypeConstraintFromCallContext(PsiExpression innerMethodCall,
-                                                                          PsiExpressionList parent,
-                                                                          PsiCallExpression contextCall,
-                                                                          PsiTypeParameter typeParameter) {
-    return null;
-  }
+public class DefaultParameterTypeInferencePolicy extends ParameterTypeInferencePolicy
+{
+	public static final DefaultParameterTypeInferencePolicy INSTANCE = new DefaultParameterTypeInferencePolicy();
 
-  @Override
-  public PsiType getDefaultExpectedType(PsiCallExpression methodCall) {
-    return PsiType.getJavaLangObject(methodCall.getManager(), methodCall.getResolveScope());
-  }
+	@Nullable
+	@Override
+	public Pair<PsiType, ConstraintType> inferTypeConstraintFromCallContext(PsiExpression innerMethodCall,
+			PsiExpressionList parent,
+			PsiCallExpression contextCall,
+			PsiTypeParameter typeParameter)
+	{
+		return null;
+	}
 
-  @Override
-  public Pair<PsiType, ConstraintType> getInferredTypeWithNoConstraint(PsiManager manager, PsiType superType) {
-    return new Pair<PsiType, ConstraintType>(superType, ConstraintType.SUBTYPE);
-  }
+	@Override
+	public PsiType getDefaultExpectedType(PsiCallExpression methodCall)
+	{
+		return PsiType.getJavaLangObject(methodCall.getManager(), methodCall.getResolveScope());
+	}
 
-  @Override
-  public PsiType adjustInferredType(PsiManager manager, PsiType guess, ConstraintType constraintType) {
-    return guess;
-  }
+	@Override
+	public Pair<PsiType, ConstraintType> getInferredTypeWithNoConstraint(PsiManager manager, PsiType superType)
+	{
+		return Pair.create(superType, ConstraintType.SUBTYPE);
+	}
+
+	@Override
+	public PsiType adjustInferredType(PsiManager manager, PsiType guess, ConstraintType constraintType)
+	{
+		return guess;
+	}
 }
