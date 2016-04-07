4b1d4f1bdd781d00317d0882e49fed34d08818a6
VISTALL <vistall.valeriy@gmail.com>
2016-04-07 16:08:58 +0300

    merge (drop useless action)

--- java-impl/src/com/intellij/codeInsight/preview/JavaPreviewHintProvider.java
+++ java-impl/src/com/intellij/codeInsight/preview/JavaPreviewHintProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,142 +15,190 @@
  */
 package com.intellij.codeInsight.preview;
 
-import com.intellij.codeInsight.intention.impl.ColorChooserIntentionAction;
+import java.awt.Color;
+
+import javax.swing.JComponent;
+
+import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.patterns.PsiExpressionPattern;
+import com.intellij.patterns.PsiJavaPatterns;
+import com.intellij.patterns.PsiMethodPattern;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.ColorSampleLookupValue;
-import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-import java.awt.*;
 
 /**
  * @author yole
  */
-public class JavaPreviewHintProvider implements PreviewHintProvider {
-  @Override
-  public boolean isSupportedFile(PsiFile file) {
-    return file instanceof PsiJavaFile;
-  }
+public class JavaPreviewHintProvider implements PreviewHintProvider
+{
+	private static final PsiMethodPattern DECODE_METHOD = PsiJavaPatterns.psiMethod().definedInClass(Color.class.getName()).withName("decode");
+	private static final PsiExpressionPattern.Capture<PsiExpression> DECODE_METHOD_CALL_PARAMETER = PsiJavaPatterns.psiExpression().methodCallParameter(0, DECODE_METHOD);
+	private static final PsiMethodPattern GET_COLOR_METHOD = PsiJavaPatterns.psiMethod().definedInClass(Color.class.getName()).withName("getColor");
+	private static final PsiExpressionPattern.Capture<PsiExpression> GET_METHOD_CALL_PARAMETER = PsiJavaPatterns.psiExpression().methodCallParameter(0, GET_COLOR_METHOD);
 
-  @Override
-  public JComponent getPreviewComponent(@NotNull PsiElement element) {
-    final PsiNewExpression psiNewExpression = PsiTreeUtil.getParentOfType(element, PsiNewExpression.class);
+	private static boolean isInsideDecodeOrGetColorMethod(PsiElement element)
+	{
+		if(element instanceof PsiJavaToken && ((PsiJavaToken) element).getTokenType() == JavaTokenType.STRING_LITERAL)
+		{
+			element = element.getParent();
+		}
 
-    if (psiNewExpression != null) {
-      final PsiJavaCodeReferenceElement referenceElement = PsiTreeUtil.getChildOfType(psiNewExpression, PsiJavaCodeReferenceElement.class);
+		return DECODE_METHOD_CALL_PARAMETER.accepts(element) || GET_METHOD_CALL_PARAMETER.accepts(element);
+	}
 
-      if (referenceElement != null) {
-        final PsiReference reference = referenceElement.getReference();
+	@Override
+	public boolean isSupportedFile(PsiFile file)
+	{
+		return file instanceof PsiJavaFile;
+	}
 
-        if (reference != null) {
-          final PsiElement psiElement = reference.resolve();
+	@Override
+	public JComponent getPreviewComponent(@NotNull PsiElement element)
+	{
+		final PsiNewExpression psiNewExpression = PsiTreeUtil.getParentOfType(element, PsiNewExpression.class);
 
-          if (psiElement instanceof PsiClass && "java.awt.Color".equals(((PsiClass)psiElement).getQualifiedName())) {
-            final PsiExpressionList argumentList = psiNewExpression.getArgumentList();
+		if(psiNewExpression != null)
+		{
+			final PsiJavaCodeReferenceElement referenceElement = PsiTreeUtil.getChildOfType(psiNewExpression, PsiJavaCodeReferenceElement.class);
 
-            if (argumentList != null) {
-              final PsiExpression[] expressions = argumentList.getExpressions();
-              int[] values = ArrayUtil.newIntArray(expressions.length);
-              float[] values2 = new float[expressions.length];
-              int i = 0;
-              int j = 0;
+			if(referenceElement != null)
+			{
+				final PsiReference reference = referenceElement.getReference();
 
-              final PsiConstantEvaluationHelper helper = JavaPsiFacade.getInstance(element.getProject()).getConstantEvaluationHelper();
-              for (final PsiExpression each : expressions) {
-                final Object o = helper.computeConstantExpression(each);
-                if (o instanceof Integer) {
-                  values[i] = ((Integer)o).intValue();
-                  if (expressions.length != 1) {
-                    values[i] = values[i] > 255 ? 255 : values[i] < 0 ? 0 : values[i];
-                  }
+				if(reference != null)
+				{
+					final PsiElement psiElement = reference.resolve();
 
-                  i++;
-                }
-                else if (o instanceof Float) {
-                  values2[j] = ((Float)o).floatValue();
-                  values2[j] = values2[j] > 1 ? 1 : values2[j] < 0 ? 0 : values2[j];
-                  j++;
-                }
-              }
+					if(psiElement instanceof PsiClass && "java.awt.Color".equals(((PsiClass) psiElement).getQualifiedName()))
+					{
+						final PsiExpressionList argumentList = psiNewExpression.getArgumentList();
+
+						if(argumentList != null)
+						{
+							final PsiExpression[] expressions = argumentList.getExpressions();
+							int[] values = ArrayUtil.newIntArray(expressions.length);
+							float[] values2 = new float[expressions.length];
+							int i = 0;
+							int j = 0;
+
+							final PsiConstantEvaluationHelper helper = JavaPsiFacade.getInstance(element.getProject()).getConstantEvaluationHelper();
+							for(final PsiExpression each : expressions)
+							{
+								final Object o = helper.computeConstantExpression(each);
+								if(o instanceof Integer)
+								{
+									values[i] = ((Integer) o).intValue();
+									if(expressions.length != 1)
+									{
+										values[i] = values[i] > 255 ? 255 : values[i] < 0 ? 0 : values[i];
+									}
+
+									i++;
+								}
+								else if(o instanceof Float)
+								{
+									values2[j] = ((Float) o).floatValue();
+									values2[j] = values2[j] > 1 ? 1 : values2[j] < 0 ? 0 : values2[j];
+									j++;
+								}
+							}
 
 
-              Color c = null;
-              if (i == expressions.length) {
-                if (i == 1 && values[0] > 255) {
-                  c = new Color(values[0]);
-                } else {
-                  switch (values.length) {
-                    case 1:
-                      c = new Color(values[0]);
-                      break;
-                    case 3:
-                      c = new Color(values[0], values[1], values[2]);
-                      break;
-                    case 4:
-                      c = new Color(values[0], values[1], values[2], values[3]);
-                      break;
-                    default:
-                      break;
-                  }
-                }
-              }
-              else if (j == expressions.length) {
-                switch (values2.length) {
-                  case 3:
-                    c = new Color(values2[0], values2[1], values2[2]);
-                    break;
-                  case 4:
-                    c = new Color(values2[0], values2[1], values2[2], values2[3]);
-                    break;
-                  default:
-                    break;
-                }
-              }
+							Color c = null;
+							if(i == expressions.length)
+							{
+								if(i == 1 && values[0] > 255)
+								{
+									c = new Color(values[0]);
+								}
+								else
+								{
+									switch(values.length)
+									{
+										case 1:
+											c = new Color(values[0]);
+											break;
+										case 3:
+											c = new Color(values[0], values[1], values[2]);
+											break;
+										case 4:
+											c = new Color(values[0], values[1], values[2], values[3]);
+											break;
+										default:
+											break;
+									}
+								}
+							}
+							else if(j == expressions.length)
+							{
+								switch(values2.length)
+								{
+									case 3:
+										c = new Color(values2[0], values2[1], values2[2]);
+										break;
+									case 4:
+										c = new Color(values2[0], values2[1], values2[2], values2[3]);
+										break;
+									default:
+										break;
+								}
+							}
 
-              if (c != null) {
-                return new ColorPreviewComponent(c);
-              }
-            }
-          }
-        }
-      }
-    }
+							if(c != null)
+							{
+								return new ColorPreviewComponent(c);
+							}
+						}
+					}
+				}
+			}
+		}
 
-    if (ColorChooserIntentionAction.isInsideDecodeOrGetColorMethod(element)) {
-      final String color = StringUtil.unquoteString(element.getText());
-      try {
-        return new ColorPreviewComponent(Color.decode(color));
-      } catch (NumberFormatException ignore) {}
-    }
+		if(isInsideDecodeOrGetColorMethod(element))
+		{
+			final String color = StringUtil.unquoteString(element.getText());
+			try
+			{
+				return new ColorPreviewComponent(Color.decode(color));
+			}
+			catch(NumberFormatException ignore)
+			{
+			}
+		}
 
-    if (PlatformPatterns.psiElement(PsiIdentifier.class).withParent(PlatformPatterns.psiElement(PsiReferenceExpression.class))
-      .accepts(element)) {
-      final PsiReference reference = element.getParent().getReference();
+		if(PlatformPatterns.psiElement(PsiIdentifier.class).withParent(PlatformPatterns.psiElement(PsiReferenceExpression.class)).accepts(element))
+		{
+			final PsiReference reference = element.getParent().getReference();
 
-      if (reference != null) {
-        final PsiElement psiElement = reference.resolve();
+			if(reference != null)
+			{
+				final PsiElement psiElement = reference.resolve();
 
-        if (psiElement instanceof PsiField) {
-          if ("java.awt.Color".equals(((PsiField)psiElement).getContainingClass().getQualifiedName())) {
-            final String colorName = ((PsiField)psiElement).getName().toLowerCase().replace("_", "");
-            final String hex = ColorSampleLookupValue.getHexCodeForColorName(colorName);
-            return new ColorPreviewComponent(Color.decode("0x" + hex.substring(1)));
-          }
-        }
-      }
-    }
+				if(psiElement instanceof PsiField)
+				{
+					if("java.awt.Color".equals(((PsiField) psiElement).getContainingClass().getQualifiedName()))
+					{
+						final String colorName = ((PsiField) psiElement).getName().toLowerCase().replace("_", "");
+						final String hex = ColorSampleLookupValue.getHexCodeForColorName(colorName);
+						return new ColorPreviewComponent(Color.decode("0x" + hex.substring(1)));
+					}
+				}
+			}
+		}
 
-    if (PlatformPatterns.psiElement().withParent(PlatformPatterns.psiElement(PsiLiteralExpression.class)).accepts(element)) {
-      final PsiLiteralExpression psiLiteralExpression = (PsiLiteralExpression) element.getParent();
-      if (psiLiteralExpression != null) {
-        return ImagePreviewComponent.getPreviewComponent(psiLiteralExpression);
-      }
-    }
+		if(PlatformPatterns.psiElement().withParent(PlatformPatterns.psiElement(PsiLiteralExpression.class)).accepts(element))
+		{
+			final PsiLiteralExpression psiLiteralExpression = (PsiLiteralExpression) element.getParent();
+			if(psiLiteralExpression != null)
+			{
+				return ImagePreviewComponent.getPreviewComponent(psiLiteralExpression);
+			}
+		}
 
-    return null;
-  }
+		return null;
+	}
 }
