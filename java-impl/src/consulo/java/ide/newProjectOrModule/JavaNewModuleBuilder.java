0aa44a40f78169bce28646508c6a84f3d4d45b3a
VISTALL <vistall.valeriy@gmail.com>
2016-09-14 23:53:09 +0300

    add empty

--- java-impl/src/consulo/java/ide/newProjectOrModule/JavaNewModuleBuilder.java
+++ java-impl/src/consulo/java/ide/newProjectOrModule/JavaNewModuleBuilder.java
@@ -16,6 +16,8 @@
 
 package consulo.java.ide.newProjectOrModule;
 
+import javax.swing.JComponent;
+
 import org.jetbrains.annotations.NotNull;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.projectRoots.JavaSdk;
@@ -25,6 +27,7 @@ import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import consulo.ide.impl.UnzipNewModuleBuilderProcessor;
 import consulo.ide.newProject.NewModuleBuilder;
+import consulo.ide.newProject.NewModuleBuilderProcessor;
 import consulo.ide.newProject.NewModuleContext;
 import consulo.java.module.extension.JavaMutableModuleExtension;
 import consulo.roots.impl.ProductionContentFolderTypeProvider;
@@ -40,6 +43,22 @@ public class JavaNewModuleBuilder implements NewModuleBuilder
 	{
 		NewModuleContext.Group group = context.createGroup("java", "Java");
 
+		group.add("Empty", AllIcons.FileTypes.Any_type, new NewModuleBuilderProcessor<JavaNewModuleBuilderPanel>()
+		{
+			@NotNull
+			@Override
+			public JavaNewModuleBuilderPanel createConfigurationPanel()
+			{
+				return new JavaNewModuleBuilderPanel();
+			}
+
+			@Override
+			public void setupModule(@NotNull JavaNewModuleBuilderPanel panel, @NotNull ContentEntry contentEntry, @NotNull ModifiableRootModel modifiableRootModel)
+			{
+				JavaNewModuleBuilder.setupModule(panel, contentEntry, modifiableRootModel);
+			}
+		});
+
 		group.add("Console Application", AllIcons.RunConfigurations.Application, new UnzipNewModuleBuilderProcessor<JavaNewModuleBuilderPanel>("/moduleTemplates/#JavaHelloWorld.zip")
 		{
 			@NotNull
@@ -50,31 +69,35 @@ public class JavaNewModuleBuilder implements NewModuleBuilder
 			}
 
 			@Override
-			public void setupModule(
-					@NotNull JavaNewModuleBuilderPanel panel, @NotNull ContentEntry contentEntry, @NotNull ModifiableRootModel modifiableRootModel)
+			public void setupModule(@NotNull JavaNewModuleBuilderPanel panel, @NotNull ContentEntry contentEntry, @NotNull ModifiableRootModel modifiableRootModel)
 			{
 				unzip(modifiableRootModel);
 
-				// need get by id - due, extension can be from original Java impl, or from other plugin, like IKVM.NET
-				JavaMutableModuleExtension<?> javaMutableModuleExtension = modifiableRootModel.getExtensionWithoutCheck("java");
-				assert javaMutableModuleExtension != null;
-
-				javaMutableModuleExtension.setEnabled(true);
-
-				Sdk sdk = panel.getSdk();
-				if(sdk != null)
-				{
-					javaMutableModuleExtension.getInheritableSdk().set(null, sdk);
-					modifiableRootModel.addModuleExtensionSdkEntry(javaMutableModuleExtension);
-
-					JavaSdkVersion version = JavaSdk.getInstance().getVersion(sdk);
-					if(version != null)
-					{
-						javaMutableModuleExtension.getInheritableLanguageLevel().set(null, version.getMaxLanguageLevel());
-					}
-				}
-				contentEntry.addFolder(contentEntry.getUrl() + "/src", ProductionContentFolderTypeProvider.getInstance());
+				JavaNewModuleBuilder.setupModule(panel, contentEntry, modifiableRootModel);
 			}
 		});
+	}
+
+	private static void setupModule(@NotNull JavaNewModuleBuilderPanel panel, @NotNull ContentEntry contentEntry, @NotNull ModifiableRootModel modifiableRootModel)
+	{
+		// need get by id - due, extension can be from original Java impl, or from other plugin, like IKVM.NET
+		JavaMutableModuleExtension<?> javaMutableModuleExtension = modifiableRootModel.getExtensionWithoutCheck("java");
+		assert javaMutableModuleExtension != null;
+
+		javaMutableModuleExtension.setEnabled(true);
+
+		Sdk sdk = panel.getSdk();
+		if(sdk != null)
+		{
+			javaMutableModuleExtension.getInheritableSdk().set(null, sdk);
+			modifiableRootModel.addModuleExtensionSdkEntry(javaMutableModuleExtension);
+
+			JavaSdkVersion version = JavaSdk.getInstance().getVersion(sdk);
+			if(version != null)
+			{
+				javaMutableModuleExtension.getInheritableLanguageLevel().set(null, version.getMaxLanguageLevel());
+			}
+		}
+		contentEntry.addFolder(contentEntry.getUrl() + "/src", ProductionContentFolderTypeProvider.getInstance());
 	}
 }
