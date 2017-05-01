a3769548b8d57b30854ca51dac374635a457ca7c
VISTALL <vistall.valeriy@gmail.com>
2017-05-01 17:08:44 +0300

    reformat

--- java-impl/src/com/intellij/ide/projectView/impl/nodes/ClassTreeNode.java
+++ java-impl/src/com/intellij/ide/projectView/impl/nodes/ClassTreeNode.java
@@ -15,195 +15,253 @@
  */
 package com.intellij.ide.projectView.impl.nodes;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.PsiClassChildrenSource;
 import com.intellij.ide.projectView.ViewSettings;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifier;
 import com.intellij.psi.util.PsiFormatUtil;
 import com.intellij.psi.util.PsiFormatUtilBase;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
+public class ClassTreeNode extends BasePsiMemberNode<PsiClass>
+{
+	public ClassTreeNode(Project project, PsiClass value, ViewSettings viewSettings)
+	{
+		super(project, value, viewSettings);
+	}
 
-public class ClassTreeNode extends BasePsiMemberNode<PsiClass>{
-  public ClassTreeNode(Project project, PsiClass value, ViewSettings viewSettings) {
-    super(project, value, viewSettings);
-  }
+	@Override
+	public Collection<AbstractTreeNode> getChildrenImpl()
+	{
+		PsiClass parent = getValue();
+		final ArrayList<AbstractTreeNode> treeNodes = new ArrayList<AbstractTreeNode>();
 
-  @Override
-  public Collection<AbstractTreeNode> getChildrenImpl() {
-    PsiClass parent = getValue();
-    final ArrayList<AbstractTreeNode> treeNodes = new ArrayList<AbstractTreeNode>();
+		if(getSettings().isShowMembers())
+		{
+			ArrayList<PsiElement> result = new ArrayList<PsiElement>();
+			PsiClassChildrenSource.DEFAULT_CHILDREN.addChildren(parent, result);
+			for(PsiElement psiElement : result)
+			{
+				if(!psiElement.isPhysical())
+				{
+					continue;
+				}
 
-    if (getSettings().isShowMembers()) {
-      ArrayList<PsiElement> result = new ArrayList<PsiElement>();
-      PsiClassChildrenSource.DEFAULT_CHILDREN.addChildren(parent, result);
-      for (PsiElement psiElement : result) {
-        if (!psiElement.isPhysical()) {
-          continue;
-        }
+				if(psiElement instanceof PsiClass)
+				{
+					treeNodes.add(new ClassTreeNode(getProject(), (PsiClass) psiElement, getSettings()));
+				}
+				else if(psiElement instanceof PsiMethod)
+				{
+					treeNodes.add(new PsiMethodNode(getProject(), (PsiMethod) psiElement, getSettings()));
+				}
+				else if(psiElement instanceof PsiField)
+				{
+					treeNodes.add(new PsiFieldNode(getProject(), (PsiField) psiElement, getSettings()));
+				}
+			}
+		}
+		return treeNodes;
+	}
 
-        if (psiElement instanceof PsiClass) {
-          treeNodes.add(new ClassTreeNode(getProject(), (PsiClass)psiElement, getSettings()));
-        }
-        else if (psiElement instanceof PsiMethod) {
-          treeNodes.add(new PsiMethodNode(getProject(), (PsiMethod)psiElement, getSettings()));
-        }
-        else if (psiElement instanceof PsiField) {
-          treeNodes.add(new PsiFieldNode(getProject(), (PsiField)psiElement, getSettings()));
-        }
-      }
-    }
-    return treeNodes;
-  }
+	@Override
+	public boolean isAlwaysLeaf()
+	{
+		return !getSettings().isShowMembers();
+	}
 
-  @Override
-  public boolean isAlwaysLeaf() {
-    return !getSettings().isShowMembers();
-  }
+	@Override
+	public void updateImpl(PresentationData data)
+	{
+		final PsiClass aClass = getValue();
+		if(aClass != null)
+		{
+			if(aClass.hasTypeParameters())
+			{
+				StringBuilder builder = new StringBuilder();
+				builder.append(aClass.getName());
+				PsiFormatUtil.formatTypeParameters(aClass, builder, PsiFormatUtilBase.SHOW_TYPE_PARAMETER_EXTENDS);
+				data.setPresentableText(builder.toString());
+			}
+			else
+			{
+				data.setPresentableText(aClass.getName());
+			}
+		}
+	}
 
-  @Override
-  public void updateImpl(PresentationData data) {
-    final PsiClass aClass = getValue();
-    if (aClass != null) {
-      if(aClass.hasTypeParameters()) {
-        StringBuilder builder  = new StringBuilder();
-        builder.append(aClass.getName());
-        PsiFormatUtil.formatTypeParameters(aClass, builder, PsiFormatUtilBase.SHOW_TYPE_PARAMETER_EXTENDS);
-        data.setPresentableText(builder.toString());
-      }
-      else {
-        data.setPresentableText(aClass.getName());
-      }
-    }
-  }
-
-  public boolean isTopLevel() {
-    return getValue() != null && getValue().getParent()instanceof PsiFile;
-  }
+	public boolean isTopLevel()
+	{
+		return getValue() != null && getValue().getParent() instanceof PsiFile;
+	}
 
 
-  @Override
-  public boolean expandOnDoubleClick() {
-    return false;
-  }
+	@Override
+	public boolean expandOnDoubleClick()
+	{
+		return false;
+	}
 
-  public PsiClass getPsiClass() {
-    return getValue();
-  }
+	public PsiClass getPsiClass()
+	{
+		return getValue();
+	}
 
-  @Override
-  public boolean isAlwaysExpand() {
-    return getParentValue() instanceof PsiFile;
-  }
+	@Override
+	public boolean isAlwaysExpand()
+	{
+		return getParentValue() instanceof PsiFile;
+	}
 
-  @Override
-  public int getWeight() {
-    return 20;
-  }
+	@Override
+	public int getWeight()
+	{
+		return 20;
+	}
 
-  @Override
-  public String getTitle() {
-    final PsiClass psiClass = getValue();
-    if (psiClass != null && psiClass.isValid()) {
-      return psiClass.getQualifiedName();
-    }
-    return super.getTitle();
-  }
+	@Override
+	public String getTitle()
+	{
+		final PsiClass psiClass = getValue();
+		if(psiClass != null && psiClass.isValid())
+		{
+			return psiClass.getQualifiedName();
+		}
+		return super.getTitle();
+	}
 
-  @Override
-  protected boolean isMarkReadOnly() {
-    return true;
-  }
+	@Override
+	protected boolean isMarkReadOnly()
+	{
+		return true;
+	}
 
-  @Override
-  public int getTypeSortWeight(final boolean sortByType) {
-    return sortByType ? 5 : 0;
-  }
+	@Override
+	public int getTypeSortWeight(final boolean sortByType)
+	{
+		return sortByType ? 5 : 0;
+	}
 
-  @Override
-  public Comparable getTypeSortKey() {
-    return new ClassNameSortKey();
-  }
+	@Override
+	public Comparable getTypeSortKey()
+	{
+		return new ClassNameSortKey();
+	}
 
-  public static int getClassPosition(final PsiClass aClass) {
-    if (aClass == null || !aClass.isValid()) {
-      return 0;
-    }
-    try {
-      int pos = 0;
-      if(aClass.isInterface()) {
-        pos = 1;
-      }
-      else if(aClass.isEnum()) {
-        pos = 2;
-      }
-      else if(aClass.hasModifierProperty(PsiModifier.ABSTRACT)) {
-        pos = -1;
-      }
-      //abstract class before concrete
-      /*if (pos == ElementPresentationUtil.CLASS_KIND_CLASS || pos == ElementPresentationUtil.CLASS_KIND_EXCEPTION) {
+	public static int getClassPosition(final PsiClass aClass)
+	{
+		if(aClass == null || !aClass.isValid())
+		{
+			return 0;
+		}
+		try
+		{
+			int pos = 0;
+			if(aClass.isInterface())
+			{
+				pos = 1;
+			}
+			else if(aClass.isEnum())
+			{
+				pos = 2;
+			}
+			else if(aClass.hasModifierProperty(PsiModifier.ABSTRACT))
+			{
+				pos = -1;
+			}
+			//abstract class before concrete
+	  /*if (pos == ElementPresentationUtil.CLASS_KIND_CLASS || pos == ElementPresentationUtil.CLASS_KIND_EXCEPTION) {
         boolean isAbstract = aClass.hasModifierProperty(PsiModifier.ABSTRACT) && !aClass.isInterface();
         if (isAbstract) {
           pos --;
         }
       } */
-      return pos;
-    }
-    catch (IndexNotReadyException e) {
-      return 0;
-    }
-  }
+			return pos;
+		}
+		catch(IndexNotReadyException e)
+		{
+			return 0;
+		}
+	}
 
-  private class ClassNameSortKey implements Comparable {
-    @Override
-    public int compareTo(final Object o) {
-      if (!(o instanceof ClassNameSortKey)) return 0;
-      ClassNameSortKey rhs = (ClassNameSortKey) o;
-      return getPosition() - rhs.getPosition();
-    }
+	private class ClassNameSortKey implements Comparable
+	{
+		@Override
+		public int compareTo(final Object o)
+		{
+			if(!(o instanceof ClassNameSortKey))
+			{
+				return 0;
+			}
+			ClassNameSortKey rhs = (ClassNameSortKey) o;
+			return getPosition() - rhs.getPosition();
+		}
 
-    int getPosition() {
-      return getClassPosition(getValue());
-    }
-  }
+		int getPosition()
+		{
+			return getClassPosition(getValue());
+		}
+	}
 
-  @Override
-  public boolean shouldDrillDownOnEmptyElement() {
-    return true;
-  }
+	@Override
+	public boolean shouldDrillDownOnEmptyElement()
+	{
+		return true;
+	}
 
-  @Override
-  public boolean canRepresent(final Object element) {
-    if (!isValid()) return false;
+	@Override
+	public boolean canRepresent(final Object element)
+	{
+		if(!isValid())
+		{
+			return false;
+		}
 
-    return super.canRepresent(element) || canRepresent(getValue(), element);
-  }
+		return super.canRepresent(element) || canRepresent(getValue(), element);
+	}
 
-  private boolean canRepresent(final PsiClass psiClass, final Object element) {
-    if (psiClass == null || !psiClass.isValid()) return false;
+	private boolean canRepresent(final PsiClass psiClass, final Object element)
+	{
+		if(psiClass == null || !psiClass.isValid())
+		{
+			return false;
+		}
 
-    final PsiFile parentFile = parentFileOf(psiClass);
-    if (parentFile != null && (parentFile == element || parentFile.getVirtualFile() == element)) return true;
+		final PsiFile parentFile = parentFileOf(psiClass);
+		if(parentFile != null && (parentFile == element || parentFile.getVirtualFile() == element))
+		{
+			return true;
+		}
 
-    if (!getSettings().isShowMembers()) {
-      if (element instanceof PsiElement && ((PsiElement)element).isValid()) {
-        PsiFile elementFile = ((PsiElement)element).getContainingFile();
-        if (elementFile != null && parentFile != null) {
-          return elementFile.equals(parentFile);
-        }
-      }
-    }
+		if(!getSettings().isShowMembers())
+		{
+			if(element instanceof PsiElement && ((PsiElement) element).isValid())
+			{
+				PsiFile elementFile = ((PsiElement) element).getContainingFile();
+				if(elementFile != null && parentFile != null)
+				{
+					return elementFile.equals(parentFile);
+				}
+			}
+		}
 
-    return false;
-  }
+		return false;
+	}
 
-  @Nullable
-  private static PsiFile parentFileOf(final PsiClass psiClass) {
-    return psiClass.getContainingClass() == null ? psiClass.getContainingFile() : null;
-  }
+	@Nullable
+	private static PsiFile parentFileOf(final PsiClass psiClass)
+	{
+		return psiClass.getContainingClass() == null ? psiClass.getContainingFile() : null;
+	}
 }
