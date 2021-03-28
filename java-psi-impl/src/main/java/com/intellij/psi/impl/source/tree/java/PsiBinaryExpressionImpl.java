505a7831e5019ecc5c39f928bc15b6b19b6a8f0d
VISTALL <vistall.valeriy@gmail.com>
2021-03-28 13:34:01 +0300

    add process declarations for poly+binary expressions

--- java-psi-impl/src/main/java/com/intellij/psi/impl/source/tree/java/PsiBinaryExpressionImpl.java
+++ java-psi-impl/src/main/java/com/intellij/psi/impl/source/tree/java/PsiBinaryExpressionImpl.java
@@ -15,144 +15,198 @@
  */
 package com.intellij.psi.impl.source.tree.java;
 
-import javax.annotation.Nonnull;
-
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.JavaResolveCache;
 import com.intellij.psi.impl.source.tree.ChildRole;
 import com.intellij.psi.impl.source.tree.ElementType;
 import com.intellij.psi.impl.source.tree.JavaElementType;
+import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.tree.ChildRoleBase;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.psi.util.TypeConversionUtil;
 import com.intellij.util.Function;
 import consulo.logging.Logger;
+import javax.annotation.Nonnull;
 
-public class PsiBinaryExpressionImpl extends ExpressionPsiElement implements PsiBinaryExpression {
-  private static final Logger LOG = Logger.getInstance(PsiBinaryExpressionImpl.class);
+public class PsiBinaryExpressionImpl extends ExpressionPsiElement implements PsiBinaryExpression
+{
+	private static final Logger LOG = Logger.getInstance(PsiBinaryExpressionImpl.class);
 
-  /** used via reflection in {@link com.intellij.psi.impl.source.tree.JavaElementType.JavaCompositeElementType#JavaCompositeElementType(java.lang.String, java.lang.Class)} */
-  @SuppressWarnings("UnusedDeclaration")
-  public PsiBinaryExpressionImpl() {
-    this(JavaElementType.BINARY_EXPRESSION);
-  }
-  protected PsiBinaryExpressionImpl(@Nonnull IElementType elementType) {
-    super(elementType);
-  }
+	/**
+	 * used via reflection in {@link com.intellij.psi.impl.source.tree.JavaElementType.JavaCompositeElementType#JavaCompositeElementType(java.lang.String, java.lang.Class)}
+	 */
+	@SuppressWarnings("UnusedDeclaration")
+	public PsiBinaryExpressionImpl()
+	{
+		this(JavaElementType.BINARY_EXPRESSION);
+	}
 
-  @Override
-  @Nonnull
-  public PsiExpression getLOperand() {
-    return (PsiExpression)findChildByRoleAsPsiElement(ChildRole.LOPERAND);
-  }
+	protected PsiBinaryExpressionImpl(@Nonnull IElementType elementType)
+	{
+		super(elementType);
+	}
 
-  @Override
-  public PsiExpression getROperand() {
-    return (PsiExpression)findChildByRoleAsPsiElement(ChildRole.ROPERAND);
-  }
+	@Override
+	@Nonnull
+	public PsiExpression getLOperand()
+	{
+		return (PsiExpression) findChildByRoleAsPsiElement(ChildRole.LOPERAND);
+	}
 
-  @Override
-  @Nonnull
-  public PsiJavaToken getOperationSign() {
-    return (PsiJavaToken)findChildByRoleAsPsiElement(ChildRole.OPERATION_SIGN);
-  }
+	@Override
+	public PsiExpression getROperand()
+	{
+		return (PsiExpression) findChildByRoleAsPsiElement(ChildRole.ROPERAND);
+	}
 
-  @Override
-  @Nonnull
-  public IElementType getOperationTokenType() {
-    return getOperationSign().getTokenType();
-  }
+	@Override
+	@Nonnull
+	public PsiJavaToken getOperationSign()
+	{
+		return (PsiJavaToken) findChildByRoleAsPsiElement(ChildRole.OPERATION_SIGN);
+	}
 
-  @Override
-  public PsiJavaToken getTokenBeforeOperand(@Nonnull PsiExpression operand) {
-    if (operand != getROperand()) return null;
-    return getOperationSign();
-  }
+	@Override
+	@Nonnull
+	public IElementType getOperationTokenType()
+	{
+		return getOperationSign().getTokenType();
+	}
 
-  private static PsiType doGetType(PsiBinaryExpressionImpl param) {
-    PsiExpression lOperand = param.getLOperand();
-    PsiExpression rOperand = param.getROperand();
-    if (rOperand == null) return null;
-    PsiType rType = rOperand.getType();
-    IElementType sign = param.getOperationTokenType();
-    // optimization: if we can calculate type based on right type only
-    PsiType type = TypeConversionUtil.calcTypeForBinaryExpression(null, rType, sign, false);
-    if (type != TypeConversionUtil.NULL_TYPE) return type;
+	@Override
+	public PsiJavaToken getTokenBeforeOperand(@Nonnull PsiExpression operand)
+	{
+		if(operand != getROperand())
+		{
+			return null;
+		}
+		return getOperationSign();
+	}
 
-    PsiType lType = lOperand.getType();
-    return TypeConversionUtil.calcTypeForBinaryExpression(lType, rType, sign, true);
-  }
+	private static PsiType doGetType(PsiBinaryExpressionImpl param)
+	{
+		PsiExpression lOperand = param.getLOperand();
+		PsiExpression rOperand = param.getROperand();
+		if(rOperand == null)
+		{
+			return null;
+		}
+		PsiType rType = rOperand.getType();
+		IElementType sign = param.getOperationTokenType();
+		// optimization: if we can calculate type based on right type only
+		PsiType type = TypeConversionUtil.calcTypeForBinaryExpression(null, rType, sign, false);
+		if(type != TypeConversionUtil.NULL_TYPE)
+		{
+			return type;
+		}
 
-  private static final Function<PsiBinaryExpressionImpl,PsiType> MY_TYPE_EVALUATOR = new Function<PsiBinaryExpressionImpl, PsiType>() {
-    @Override
-    public PsiType fun(PsiBinaryExpressionImpl expression) {
-      return doGetType(expression);
-    }
-  };
-  @Override
-  public PsiType getType() {
-    return JavaResolveCache.getInstance(getProject()).getType(this, MY_TYPE_EVALUATOR);
-  }
+		PsiType lType = lOperand.getType();
+		return TypeConversionUtil.calcTypeForBinaryExpression(lType, rType, sign, true);
+	}
 
-  @Override
-  public ASTNode findChildByRole(int role) {
-    LOG.assertTrue(ChildRole.isUnique(role));
-    switch (role) {
-      default:
-        return null;
+	private static final Function<PsiBinaryExpressionImpl, PsiType> MY_TYPE_EVALUATOR = new Function<PsiBinaryExpressionImpl, PsiType>()
+	{
+		@Override
+		public PsiType fun(PsiBinaryExpressionImpl expression)
+		{
+			return doGetType(expression);
+		}
+	};
 
-      case ChildRole.LOPERAND:
-        return getFirstChildNode();
+	@Override
+	public PsiType getType()
+	{
+		return JavaResolveCache.getInstance(getProject()).getType(this, MY_TYPE_EVALUATOR);
+	}
 
-      case ChildRole.ROPERAND:
-        return ElementType.EXPRESSION_BIT_SET.contains(getLastChildNode().getElementType()) ? getLastChildNode() : null;
+	@Override
+	public ASTNode findChildByRole(int role)
+	{
+		LOG.assertTrue(ChildRole.isUnique(role));
+		switch(role)
+		{
+			default:
+				return null;
 
-      case ChildRole.OPERATION_SIGN:
-        return findChildByType(OUR_OPERATIONS_BIT_SET);
-    }
-  }
+			case ChildRole.LOPERAND:
+				return getFirstChildNode();
 
-  @Override
-  public int getChildRole(ASTNode child) {
-    LOG.assertTrue(child.getTreeParent() == this);
-    if (ElementType.EXPRESSION_BIT_SET.contains(child.getElementType())) {
-      if (child == getFirstChildNode()) return ChildRole.LOPERAND;
-      if (child == getLastChildNode()) return ChildRole.ROPERAND;
-      return ChildRoleBase.NONE;
-    }
-    if (OUR_OPERATIONS_BIT_SET.contains(child.getElementType())) {
-      return ChildRole.OPERATION_SIGN;
-    }
-    return ChildRoleBase.NONE;
-  }
+			case ChildRole.ROPERAND:
+				return ElementType.EXPRESSION_BIT_SET.contains(getLastChildNode().getElementType()) ? getLastChildNode() : null;
 
-  private static final TokenSet OUR_OPERATIONS_BIT_SET =
-    TokenSet.create(JavaTokenType.OROR, JavaTokenType.ANDAND, JavaTokenType.OR, JavaTokenType.XOR, JavaTokenType.AND, JavaTokenType.EQEQ,
-                    JavaTokenType.NE, JavaTokenType.LT, JavaTokenType.GT, JavaTokenType.LE, JavaTokenType.GE, JavaTokenType.LTLT,
-                    JavaTokenType.GTGT, JavaTokenType.GTGTGT, JavaTokenType.PLUS, JavaTokenType.MINUS, JavaTokenType.ASTERISK, JavaTokenType.DIV,
-                    JavaTokenType.PERC);
+			case ChildRole.OPERATION_SIGN:
+				return findChildByType(OUR_OPERATIONS_BIT_SET);
+		}
+	}
 
-  @Override
-  public void accept(@Nonnull PsiElementVisitor visitor) {
-    if (visitor instanceof JavaElementVisitor) {
-      ((JavaElementVisitor)visitor).visitBinaryExpression(this);
-    }
-    else {
-      visitor.visitElement(this);
-    }
-  }
+	@Override
+	public int getChildRole(ASTNode child)
+	{
+		LOG.assertTrue(child.getTreeParent() == this);
+		if(ElementType.EXPRESSION_BIT_SET.contains(child.getElementType()))
+		{
+			if(child == getFirstChildNode())
+			{
+				return ChildRole.LOPERAND;
+			}
+			if(child == getLastChildNode())
+			{
+				return ChildRole.ROPERAND;
+			}
+			return ChildRoleBase.NONE;
+		}
+		if(OUR_OPERATIONS_BIT_SET.contains(child.getElementType()))
+		{
+			return ChildRole.OPERATION_SIGN;
+		}
+		return ChildRoleBase.NONE;
+	}
 
-  public String toString() {
-    return "PsiBinaryExpression:" + getText();
-  }
+	private static final TokenSet OUR_OPERATIONS_BIT_SET =
+			TokenSet.create(JavaTokenType.OROR, JavaTokenType.ANDAND, JavaTokenType.OR, JavaTokenType.XOR, JavaTokenType.AND, JavaTokenType.EQEQ,
+					JavaTokenType.NE, JavaTokenType.LT, JavaTokenType.GT, JavaTokenType.LE, JavaTokenType.GE, JavaTokenType.LTLT,
+					JavaTokenType.GTGT, JavaTokenType.GTGTGT, JavaTokenType.PLUS, JavaTokenType.MINUS, JavaTokenType.ASTERISK, JavaTokenType.DIV,
+					JavaTokenType.PERC);
 
-  @Nonnull
-  @Override
-  public PsiExpression[] getOperands() {
-    PsiExpression rOperand = getROperand();
-    return rOperand == null ? new PsiExpression[]{getLOperand()} : new PsiExpression[]{getLOperand(), rOperand};
-  }
+	@Override
+	public void accept(@Nonnull PsiElementVisitor visitor)
+	{
+		if(visitor instanceof JavaElementVisitor)
+		{
+			((JavaElementVisitor) visitor).visitBinaryExpression(this);
+		}
+		else
+		{
+			visitor.visitElement(this);
+		}
+	}
+
+	public String toString()
+	{
+		return "PsiBinaryExpression:" + getText();
+	}
+
+
+	@Override
+	public boolean processDeclarations(@Nonnull PsiScopeProcessor processor,
+									   @Nonnull ResolveState state,
+									   PsiElement lastParent,
+									   @Nonnull PsiElement place)
+	{
+		return PsiPolyadicExpressionImpl.processDeclarations(this, processor, state, lastParent, place);
+	}
+
+	@Nonnull
+	@Override
+	public PsiExpression[] getOperands()
+	{
+		PsiExpression rOperand = getROperand();
+		return rOperand == null ? new PsiExpression[]{getLOperand()} : new PsiExpression[]{
+				getLOperand(),
+				rOperand
+		};
+	}
 }
 
