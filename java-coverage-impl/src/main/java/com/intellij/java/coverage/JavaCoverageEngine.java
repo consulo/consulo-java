a5c54c830f65a227ac1177827ee3f6a5bd4f64f5
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-01-22 14:09:18 +0400

    remove order last since we can't control it

--- java-coverage-impl/src/main/java/com/intellij/java/coverage/JavaCoverageEngine.java
+++ java-coverage-impl/src/main/java/com/intellij/java/coverage/JavaCoverageEngine.java
@@ -55,7 +55,7 @@ import java.util.*;
 /**
  * @author Roman.Chernyatchik
  */
-@ExtensionImpl(order = "last")
+@ExtensionImpl
 public class JavaCoverageEngine extends CoverageEngine {
   private static final Logger LOG = Logger.getInstance(JavaCoverageEngine.class);
 
