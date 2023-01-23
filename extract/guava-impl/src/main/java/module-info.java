16871069999ab668ffed5893ddbc6011f3199e88
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-01-23 13:24:31 +0400

    add open

--- extract/guava-impl/src/main/java/module-info.java
+++ extract/guava-impl/src/main/java/module-info.java
@@ -6,4 +6,7 @@ module consulo.java.guava.impl {
     requires consulo.java.language.impl;
     // TODO remove in future
     requires java.desktop;
+
+    // need open for CacheValueManager analyze
+    opens consulo.java.guava to consulo.application.impl;
 }
