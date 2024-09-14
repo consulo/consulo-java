488c4a555eacba238f9bbbe9432b34e52fa03021
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-09-14 18:45:03 +0400

    reformat

--- plugin/src/main/java/com/intellij/java/impl/refactoring/ui/JavaVisibilityPanel.java
+++ plugin/src/main/java/com/intellij/java/impl/refactoring/ui/JavaVisibilityPanel.java
@@ -26,7 +26,6 @@ package com.intellij.java.impl.refactoring.ui;
 
 import com.intellij.java.language.psi.PsiModifier;
 import com.intellij.java.language.util.VisibilityUtil;
-import consulo.language.editor.refactoring.RefactoringBundle;
 import consulo.ide.impl.idea.refactoring.ui.VisibilityPanelBase;
 import consulo.language.editor.refactoring.localize.RefactoringLocalize;
 import consulo.ui.ex.awt.IdeBorderFactory;
@@ -40,130 +39,135 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class JavaVisibilityPanel extends VisibilityPanelBase<String> {
-  private JRadioButton myRbAsIs;
-  private JRadioButton myRbEscalate;
-  private final JRadioButton myRbPrivate;
-  private final JRadioButton myRbProtected;
-  private final JRadioButton myRbPackageLocal;
-  private final JRadioButton myRbPublic;
+    private JRadioButton myRbAsIs;
+    private JRadioButton myRbEscalate;
+    private final JRadioButton myRbPrivate;
+    private final JRadioButton myRbProtected;
+    private final JRadioButton myRbPackageLocal;
+    private final JRadioButton myRbPublic;
 
-  public JavaVisibilityPanel(boolean hasAsIs, final boolean hasEscalate) {
-    setBorder(IdeBorderFactory.createTitledBorder(
-      RefactoringLocalize.visibilityBorderTitle().get(),
-      true,
-      new Insets(
-        IdeBorderFactory.TITLED_BORDER_TOP_INSET,
-        UIUtil.DEFAULT_HGAP,
-        IdeBorderFactory.TITLED_BORDER_BOTTOM_INSET,
-        IdeBorderFactory.TITLED_BORDER_RIGHT_INSET
-      )
-    ));
-    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-    ButtonGroup bg = new ButtonGroup();
+    public JavaVisibilityPanel(boolean hasAsIs, final boolean hasEscalate) {
+        setBorder(IdeBorderFactory.createTitledBorder(
+            RefactoringLocalize.visibilityBorderTitle().get(),
+            true,
+            new Insets(
+                IdeBorderFactory.TITLED_BORDER_TOP_INSET,
+                UIUtil.DEFAULT_HGAP,
+                IdeBorderFactory.TITLED_BORDER_BOTTOM_INSET,
+                IdeBorderFactory.TITLED_BORDER_RIGHT_INSET
+            )
+        ));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        ButtonGroup bg = new ButtonGroup();
 
-    ItemListener listener = new ItemListener() {
-      public void itemStateChanged(ItemEvent e) {
-        if (e.getStateChange() == ItemEvent.SELECTED) {
-          myEventDispatcher.getMulticaster().stateChanged(new ChangeEvent(this));
+        ItemListener listener = new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    myEventDispatcher.getMulticaster().stateChanged(new ChangeEvent(this));
+                }
+            }
+        };
+
+        if (hasEscalate) {
+            myRbEscalate = new JRadioButton();
+            myRbEscalate.setText(RefactoringLocalize.visibilityEscalate().get());
+            myRbEscalate.addItemListener(listener);
+            add(myRbEscalate);
+            bg.add(myRbEscalate);
         }
-      }
-    };
 
-    if (hasEscalate) {
-      myRbEscalate = new JRadioButton();
-      myRbEscalate.setText(RefactoringLocalize.visibilityEscalate().get());
-      myRbEscalate.addItemListener(listener);
-      add(myRbEscalate);
-      bg.add(myRbEscalate);
+        if (hasAsIs) {
+            myRbAsIs = new JRadioButton();
+            myRbAsIs.setText(RefactoringLocalize.visibilityAsIs().get());
+            myRbAsIs.addItemListener(listener);
+            add(myRbAsIs);
+            bg.add(myRbAsIs);
+        }
+
+
+        myRbPrivate = new JRadioButton();
+        myRbPrivate.setText(RefactoringLocalize.visibilityPrivate().get());
+        myRbPrivate.addItemListener(listener);
+        myRbPrivate.setFocusable(false);
+        add(myRbPrivate);
+        bg.add(myRbPrivate);
+
+        myRbPackageLocal = new JRadioButton();
+        myRbPackageLocal.setText(RefactoringLocalize.visibilityPackageLocal().get());
+        myRbPackageLocal.addItemListener(listener);
+        myRbPackageLocal.setFocusable(false);
+        add(myRbPackageLocal);
+        bg.add(myRbPackageLocal);
+
+        myRbProtected = new JRadioButton();
+        myRbProtected.setText(RefactoringLocalize.visibilityProtected().get());
+        myRbProtected.addItemListener(listener);
+        myRbProtected.setFocusable(false);
+        add(myRbProtected);
+        bg.add(myRbProtected);
+
+        myRbPublic = new JRadioButton();
+        myRbPublic.setText(RefactoringLocalize.visibilityPublic().get());
+        myRbPublic.addItemListener(listener);
+        myRbPublic.setFocusable(false);
+        add(myRbPublic);
+        bg.add(myRbPublic);
     }
 
-    if (hasAsIs) {
-      myRbAsIs = new JRadioButton();
-      myRbAsIs.setText(RefactoringLocalize.visibilityAsIs().get());
-      myRbAsIs.addItemListener(listener);
-      add(myRbAsIs);
-      bg.add(myRbAsIs);
+
+    @Nullable
+    public String getVisibility() {
+        if (myRbPublic.isSelected()) {
+            return PsiModifier.PUBLIC;
+        }
+        if (myRbPackageLocal.isSelected()) {
+            return PsiModifier.PACKAGE_LOCAL;
+        }
+        if (myRbProtected.isSelected()) {
+            return PsiModifier.PROTECTED;
+        }
+        if (myRbPrivate.isSelected()) {
+            return PsiModifier.PRIVATE;
+        }
+        if (myRbEscalate != null && myRbEscalate.isSelected()) {
+            return VisibilityUtil.ESCALATE_VISIBILITY;
+        }
+
+        return null;
     }
 
-
-    myRbPrivate = new JRadioButton();
-    myRbPrivate.setText(RefactoringLocalize.visibilityPrivate().get());
-    myRbPrivate.addItemListener(listener);
-    myRbPrivate.setFocusable(false);
-    add(myRbPrivate);
-    bg.add(myRbPrivate);
-
-    myRbPackageLocal = new JRadioButton();
-    myRbPackageLocal.setText(RefactoringLocalize.visibilityPackageLocal().get());
-    myRbPackageLocal.addItemListener(listener);
-    myRbPackageLocal.setFocusable(false);
-    add(myRbPackageLocal);
-    bg.add(myRbPackageLocal);
-
-    myRbProtected = new JRadioButton();
-    myRbProtected.setText(RefactoringLocalize.visibilityProtected().get());
-    myRbProtected.addItemListener(listener);
-    myRbProtected.setFocusable(false);
-    add(myRbProtected);
-    bg.add(myRbProtected);
-
-    myRbPublic = new JRadioButton();
-    myRbPublic.setText(RefactoringLocalize.visibilityPublic().get());
-    myRbPublic.addItemListener(listener);
-    myRbPublic.setFocusable(false);
-    add(myRbPublic);
-    bg.add(myRbPublic);
-  }
-
-
-  @Nullable
-  public String getVisibility() {
-    if (myRbPublic.isSelected()) {
-      return PsiModifier.PUBLIC;
-    }
-    if (myRbPackageLocal.isSelected()) {
-      return PsiModifier.PACKAGE_LOCAL;
-    }
-    if (myRbProtected.isSelected()) {
-      return PsiModifier.PROTECTED;
-    }
-    if (myRbPrivate.isSelected()) {
-      return PsiModifier.PRIVATE;
-    }
-    if (myRbEscalate != null && myRbEscalate.isSelected()) {
-      return VisibilityUtil.ESCALATE_VISIBILITY;
+    public void setVisibility(@Nullable String visibility) {
+        if (PsiModifier.PUBLIC.equals(visibility)) {
+            myRbPublic.setSelected(true);
+        }
+        else if (PsiModifier.PROTECTED.equals(visibility)) {
+            myRbProtected.setSelected(true);
+        }
+        else if (PsiModifier.PACKAGE_LOCAL.equals(visibility)) {
+            myRbPackageLocal.setSelected(true);
+        }
+        else if (PsiModifier.PRIVATE.equals(visibility)) {
+            myRbPrivate.setSelected(true);
+        }
+        else if (myRbEscalate != null) {
+            myRbEscalate.setSelected(true);
+        }
+        else if (myRbAsIs != null) {
+            myRbAsIs.setSelected(true);
+        }
     }
 
-    return null;
-  }
-
-  public void setVisibility(@Nullable String visibility) {
-    if (PsiModifier.PUBLIC.equals(visibility)) {
-      myRbPublic.setSelected(true);
-    } else if (PsiModifier.PROTECTED.equals(visibility)) {
-      myRbProtected.setSelected(true);
-    } else if (PsiModifier.PACKAGE_LOCAL.equals(visibility)) {
-      myRbPackageLocal.setSelected(true);
-    } else if (PsiModifier.PRIVATE.equals(visibility)) {
-      myRbPrivate.setSelected(true);
-    } else if (myRbEscalate != null) {
-      myRbEscalate.setSelected(true);
-    } else if (myRbAsIs != null) {
-      myRbAsIs.setSelected(true);
+    public void disableAllButPublic() {
+        myRbPrivate.setEnabled(false);
+        myRbProtected.setEnabled(false);
+        myRbPackageLocal.setEnabled(false);
+        if (myRbEscalate != null) {
+            myRbEscalate.setEnabled(false);
+        }
+        if (myRbAsIs != null) {
+            myRbAsIs.setEnabled(false);
+        }
+        myRbPublic.setEnabled(true);
+        myRbPublic.setSelected(true);
     }
-  }
-
-  public void disableAllButPublic() {
-    myRbPrivate.setEnabled(false);
-    myRbProtected.setEnabled(false);
-    myRbPackageLocal.setEnabled(false);
-    if (myRbEscalate != null) {
-      myRbEscalate.setEnabled(false);
-    }
-    if (myRbAsIs != null) {
-      myRbAsIs.setEnabled(false);
-    }
-    myRbPublic.setEnabled(true);
-    myRbPublic.setSelected(true);
-  }
 }
