0dc820815650813ebd3d00122257c3d62b6ae91d
UNV <nikolay@yurchenko.su>
2024-10-30 19:48:58 +0300

    Reformatting module-info.java and pom.xml in java-analysis-api, java-compiler-impl, java-language-api, java-language-impl.

--- java-analysis-api/src/main/java/module-info.java
+++ java-analysis-api/src/main/java/module-info.java
@@ -3,19 +3,19 @@
  * @since 02/12/2022
  */
 module consulo.java.analysis.api {
-  requires transitive consulo.java.language.api;
+    requires transitive consulo.java.language.api;
 
-  exports com.intellij.java.analysis;
-  exports com.intellij.java.analysis.codeInsight.daemon;
-  exports com.intellij.java.analysis.codeInsight.guess;
-  exports com.intellij.java.analysis.codeInsight.intention;
-  exports com.intellij.java.analysis.codeInspection;
-  exports com.intellij.java.analysis.codeInspection.ex;
-  exports com.intellij.java.analysis.codeInspection.reference;
-  exports com.intellij.java.analysis.property;
-  exports com.intellij.java.analysis.refactoring;
-  exports consulo.java.analysis.codeInsight;
-  exports consulo.java.analysis.codeInspection;
+    exports com.intellij.java.analysis;
+    exports com.intellij.java.analysis.codeInsight.daemon;
+    exports com.intellij.java.analysis.codeInsight.guess;
+    exports com.intellij.java.analysis.codeInsight.intention;
+    exports com.intellij.java.analysis.codeInspection;
+    exports com.intellij.java.analysis.codeInspection.ex;
+    exports com.intellij.java.analysis.codeInspection.reference;
+    exports com.intellij.java.analysis.property;
+    exports com.intellij.java.analysis.refactoring;
+    exports consulo.java.analysis.codeInsight;
+    exports consulo.java.analysis.codeInspection;
 
-  exports consulo.java.deadCodeNotWorking;
+    exports consulo.java.deadCodeNotWorking;
 }
