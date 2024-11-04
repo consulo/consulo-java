43621bd522b8e5783478dd1064fb62bbafae378a
UNV <nikolay@yurchenko.su>
2024-11-04 06:27:28 +0300

    Localizing users of JavaAnalysisBundle. Refactoring to reduce number of inspection warnings.

--- java-analysis-impl/src/main/java/com/intellij/java/analysis/impl/codeInspection/dataFlow/Mutability.java
+++ java-analysis-impl/src/main/java/com/intellij/java/analysis/impl/codeInspection/dataFlow/Mutability.java
@@ -3,7 +3,6 @@
  */
 package com.intellij.java.analysis.impl.codeInspection.dataFlow;
 
-import com.intellij.java.analysis.JavaAnalysisBundle;
 import com.intellij.java.analysis.impl.codeInspection.dataFlow.inference.JavaSourceInference;
 import com.intellij.java.analysis.impl.codeInspection.dataFlow.types.DfReferenceType;
 import com.intellij.java.analysis.impl.codeInspection.dataFlow.types.DfType;
@@ -13,25 +12,25 @@ import com.intellij.java.language.impl.psi.impl.source.PsiMethodImpl;
 import com.intellij.java.language.psi.*;
 import com.siyeh.ig.psiutils.ClassUtils;
 import com.siyeh.ig.psiutils.ExpressionUtils;
+import consulo.annotation.access.RequiredReadAction;
 import consulo.application.util.CachedValue;
 import consulo.application.util.CachedValueProvider;
 import consulo.application.util.CachedValuesManager;
 import consulo.component.util.ModificationTracker;
+import consulo.java.analysis.localize.JavaAnalysisLocalize;
 import consulo.language.file.light.LightVirtualFile;
 import consulo.language.impl.psi.LightElement;
 import consulo.language.psi.PsiModificationTracker;
 import consulo.language.psi.util.LanguageCachedValueUtil;
 import consulo.language.psi.util.PsiTreeUtil;
+import consulo.localize.LocalizeValue;
 import consulo.project.Project;
 import consulo.util.collection.ContainerUtil;
 import consulo.util.dataholder.Key;
 import consulo.util.lang.ObjectUtil;
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import one.util.streamex.StreamEx;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.PropertyKey;
-
-import jakarta.annotation.Nonnull;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -41,37 +40,34 @@ public enum Mutability {
     /**
      * Mutability is not known; probably value can be mutated
      */
-    UNKNOWN("mutability.unknown", null),
+    UNKNOWN(JavaAnalysisLocalize.mutabilityUnknown(), null),
     /**
      * A value is known to be mutable (e.g. elements are sometimes added to the collection)
      */
-    MUTABLE("mutability.modifiable", null),
+    MUTABLE(JavaAnalysisLocalize.mutabilityModifiable(), null),
     /**
      * A value is known to be an immutable view over a possibly mutable value: it cannot be mutated directly using this
      * reference; however subsequent reads (e.g. {@link Collection#size}) may return different results if the
      * underlying value is mutated by somebody else.
      */
-    UNMODIFIABLE_VIEW("mutability.unmodifiable.view", "org.jetbrains.annotations.UnmodifiableView"),
+    UNMODIFIABLE_VIEW(JavaAnalysisLocalize.mutabilityUnmodifiableView(), "org.jetbrains.annotations.UnmodifiableView"),
     /**
      * A value is known to be immutable. For collection no elements could be added, removed or altered (though if collection
      * contains mutable elements, they still could be mutated).
      */
-    UNMODIFIABLE("mutability.unmodifiable", "org.jetbrains.annotations.Unmodifiable");
+    UNMODIFIABLE(JavaAnalysisLocalize.mutabilityUnmodifiable(), "org.jetbrains.annotations.Unmodifiable");
 
-    public static final
     @Nonnull
-    String UNMODIFIABLE_ANNOTATION = UNMODIFIABLE.myAnnotation;
-    public static final
+    public static final String UNMODIFIABLE_ANNOTATION = UNMODIFIABLE.myAnnotation;
     @Nonnull
-    String UNMODIFIABLE_VIEW_ANNOTATION = UNMODIFIABLE_VIEW.myAnnotation;
-    private final
-    @PropertyKey(resourceBundle = JavaAnalysisBundle.BUNDLE)
-    String myResourceKey;
+    public static final String UNMODIFIABLE_VIEW_ANNOTATION = UNMODIFIABLE_VIEW.myAnnotation;
+    @Nonnull
+    private final LocalizeValue myPresentationName;
     private final String myAnnotation;
     private final Key<CachedValue<PsiAnnotation>> myKey;
 
-    Mutability(@PropertyKey(resourceBundle = JavaAnalysisBundle.BUNDLE) String resourceKey, String annotation) {
-        myResourceKey = resourceKey;
+    Mutability(@Nonnull LocalizeValue presentationName, String annotation) {
+        myPresentationName = presentationName;
         myAnnotation = annotation;
         myKey = annotation == null ? null : Key.create(annotation);
     }
@@ -80,11 +76,9 @@ public enum Mutability {
         return DfTypes.customObject(TypeConstraints.TOP, DfaNullability.UNKNOWN, this, null, DfTypes.BOTTOM);
     }
 
-    public
     @Nonnull
-    @Nls
-    String getPresentationName() {
-        return JavaAnalysisBundle.message(myResourceKey);
+    public String getPresentationName() {
+        return myPresentationName.get();
     }
 
     public boolean isUnmodifiable() {
@@ -164,12 +158,12 @@ public enum Mutability {
     }
 
     @Nonnull
+    @RequiredReadAction
     private static Mutability calcMutability(@Nonnull PsiModifierListOwner owner) {
-        if (owner instanceof PsiParameter && owner.getParent() instanceof PsiParameterList) {
-            PsiParameterList list = (PsiParameterList)owner.getParent();
+        if (owner instanceof PsiParameter parameter && parameter.getParent() instanceof PsiParameterList list) {
             PsiMethod method = ObjectUtil.tryCast(list.getParent(), PsiMethod.class);
             if (method != null) {
-                int index = list.getParameterIndex((PsiParameter)owner);
+                int index = list.getParameterIndex(parameter);
                 JavaMethodContractUtil.ContractInfo contractInfo = JavaMethodContractUtil.getContractInfo(method);
                 if (contractInfo.isExplicit()) {
                     MutationSignature signature = contractInfo.getMutationSignature();
@@ -216,8 +210,8 @@ public enum Mutability {
                 if (ClassUtils.isImmutable(initializer.getType())) {
                     newMutability = UNMODIFIABLE;
                 }
-                else if (initializer instanceof PsiMethodCallExpression) {
-                    PsiMethod method = ((PsiMethodCallExpression)initializer).resolveMethod();
+                else if (initializer instanceof PsiMethodCallExpression methodCall) {
+                    PsiMethod method = methodCall.resolveMethod();
                     newMutability = method == null ? UNKNOWN : getMutability(method);
                 }
                 mutability = mutability.unite(newMutability);
@@ -227,10 +221,10 @@ public enum Mutability {
             }
             return mutability;
         }
-        return owner instanceof PsiMethodImpl ? JavaSourceInference.inferMutability((PsiMethodImpl)owner) : UNKNOWN;
+        return owner instanceof PsiMethodImpl method ? JavaSourceInference.inferMutability(method) : UNKNOWN;
     }
 
     public static Mutability fromDfType(DfType dfType) {
-        return dfType instanceof DfReferenceType ? ((DfReferenceType)dfType).getMutability() : UNKNOWN;
+        return dfType instanceof DfReferenceType referenceType ? referenceType.getMutability() : UNKNOWN;
     }
 }
