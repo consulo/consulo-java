0c6a79cc3618c8b0bf29feac2835ee2e474b32cb
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-01-05 16:19:58 +0400

    missed changed from PsiMethodUtil

--- java-language-api/src/main/java/com/intellij/java/language/psi/util/PsiMethodUtil.java
+++ java-language-api/src/main/java/com/intellij/java/language/psi/util/PsiMethodUtil.java
@@ -18,6 +18,7 @@ package com.intellij.java.language.psi.util;
 
 import com.intellij.java.language.codeInsight.runner.JavaMainMethodProvider;
 import com.intellij.java.language.psi.*;
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
 import java.util.function.Predicate;
@@ -26,64 +27,97 @@ import java.util.function.Predicate;
  * @author mike
  */
 public class PsiMethodUtil {
-  public static final Predicate<PsiClass> MAIN_CLASS = psiClass -> {
-    if (psiClass instanceof PsiAnonymousClass) return false;
-    if (psiClass.isInterface()) return false;
-    return psiClass.getContainingClass() == null || psiClass.hasModifierProperty(PsiModifier.STATIC);
-  };
+    public static final Predicate<PsiClass> MAIN_CLASS = psiClass -> {
+        if (psiClass instanceof PsiAnonymousClass) {
+            return false;
+        }
+        if (psiClass.isInterface()) {
+            return false;
+        }
+        return psiClass.getContainingClass() == null || psiClass.hasModifierProperty(PsiModifier.STATIC);
+    };
 
-  private PsiMethodUtil() {
-  }
-
-  @Nullable
-  public static PsiMethod findMainMethod(final PsiClass aClass) {
-    for (JavaMainMethodProvider provider : JavaMainMethodProvider.EP_NAME.getExtensionList()) {
-      if (provider.isApplicable(aClass)) {
-        return provider.findMainInClass(aClass);
-      }
+    private PsiMethodUtil() {
     }
-    final PsiMethod[] mainMethods = aClass.findMethodsByName("main", true);
-    return findMainMethod(mainMethods);
-  }
 
-  @Nullable
-  private static PsiMethod findMainMethod(final PsiMethod[] mainMethods) {
-    for (final PsiMethod mainMethod : mainMethods) {
-      if (isMainMethod(mainMethod)) return mainMethod;
+    @Nullable
+    public static PsiMethod findMainMethod(final PsiClass aClass) {
+        for (JavaMainMethodProvider provider : JavaMainMethodProvider.EP_NAME.getExtensionList()) {
+            if (provider.isApplicable(aClass)) {
+                return provider.findMainInClass(aClass);
+            }
+        }
+        final PsiMethod[] mainMethods = aClass.findMethodsByName("main", true);
+        return findMainMethod(mainMethods);
     }
-    return null;
-  }
 
-  public static boolean isMainMethod(final PsiMethod method) {
-    if (method == null || method.getContainingClass() == null) return false;
-    if (!PsiType.VOID.equals(method.getReturnType())) return false;
-    if (!method.hasModifierProperty(PsiModifier.STATIC)) return false;
-    if (!method.hasModifierProperty(PsiModifier.PUBLIC)) return false;
-    final PsiParameter[] parameters = method.getParameterList().getParameters();
-    if (parameters.length != 1) return false;
-    final PsiType type = parameters[0].getType();
-    if (!(type instanceof PsiArrayType)) return false;
-    final PsiType componentType = ((PsiArrayType)type).getComponentType();
-    return componentType.equalsToText("java.lang.String");
-  }
-
-  public static boolean hasMainMethod(final PsiClass psiClass) {
-    for (JavaMainMethodProvider provider : JavaMainMethodProvider.EP_NAME.getExtensionList()) {
-      if (provider.isApplicable(psiClass)) {
-        return provider.hasMainMethod(psiClass);
-      }
+    @Nullable
+    private static PsiMethod findMainMethod(final PsiMethod[] mainMethods) {
+        for (final PsiMethod mainMethod : mainMethods) {
+            if (isMainMethod(mainMethod)) {
+                return mainMethod;
+            }
+        }
+        return null;
     }
-    return findMainMethod(psiClass.findMethodsByName("main", true)) != null;
-  }
 
-  @Nullable
-  public static PsiMethod findMainInClass(final PsiClass aClass) {
-    if (!MAIN_CLASS.test(aClass)) return null;
-    for (JavaMainMethodProvider provider : JavaMainMethodProvider.EP_NAME.getExtensionList()) {
-      if (provider.isApplicable(aClass)) {
-        return provider.findMainInClass(aClass);
-      }
+    public static boolean isMainMethod(final PsiMethod method) {
+        if (method == null || method.getContainingClass() == null) {
+            return false;
+        }
+        if (!PsiType.VOID.equals(method.getReturnType())) {
+            return false;
+        }
+        if (!method.hasModifierProperty(PsiModifier.STATIC)) {
+            return false;
+        }
+        if (!method.hasModifierProperty(PsiModifier.PUBLIC)) {
+            return false;
+        }
+        final PsiParameter[] parameters = method.getParameterList().getParameters();
+        if (parameters.length != 1) {
+            return false;
+        }
+        final PsiType type = parameters[0].getType();
+        if (!(type instanceof PsiArrayType)) {
+            return false;
+        }
+        final PsiType componentType = ((PsiArrayType) type).getComponentType();
+        return componentType.equalsToText("java.lang.String");
     }
-    return findMainMethod(aClass);
-  }
+
+    public static boolean hasMainMethod(final PsiClass psiClass) {
+        for (JavaMainMethodProvider provider : JavaMainMethodProvider.EP_NAME.getExtensionList()) {
+            if (provider.isApplicable(psiClass)) {
+                return provider.hasMainMethod(psiClass);
+            }
+        }
+        return findMainMethod(psiClass.findMethodsByName("main", true)) != null;
+    }
+
+    /**
+     * Determines if the given class has a main method and can be launched.
+     *
+     * @param aClass the class to check for a main method.
+     * @return true if the class has a main method, false otherwise.
+     */
+    public static boolean hasMainInClass(@Nonnull final PsiClass aClass) {
+        if (!MAIN_CLASS.test(aClass)) {
+            return false;
+        }
+        return hasMainMethod(aClass);
+    }
+
+    @Nullable
+    public static PsiMethod findMainInClass(final PsiClass aClass) {
+        if (!MAIN_CLASS.test(aClass)) {
+            return null;
+        }
+        for (JavaMainMethodProvider provider : JavaMainMethodProvider.EP_NAME.getExtensionList()) {
+            if (provider.isApplicable(aClass)) {
+                return provider.findMainInClass(aClass);
+            }
+        }
+        return findMainMethod(aClass);
+    }
 }
