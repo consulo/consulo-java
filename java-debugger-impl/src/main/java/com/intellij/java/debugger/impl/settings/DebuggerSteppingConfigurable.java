bb94c4497eff11809f8e385057348eb2328cf113
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-04 21:22:55 +0400

    fix compile - api changed

--- java-debugger-impl/src/main/java/com/intellij/java/debugger/impl/settings/DebuggerSteppingConfigurable.java
+++ java-debugger-impl/src/main/java/com/intellij/java/debugger/impl/settings/DebuggerSteppingConfigurable.java
@@ -19,96 +19,83 @@ import com.intellij.java.debugger.DebuggerBundle;
 import com.intellij.java.debugger.impl.classFilter.ClassFilterEditor;
 import com.intellij.java.debugger.impl.ui.JavaDebuggerSupport;
 import consulo.configurable.IdeaConfigurableUi;
-
+import consulo.disposer.Disposable;
+import consulo.ui.ex.awt.JBUI;
 import jakarta.annotation.Nonnull;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-class DebuggerSteppingConfigurable implements IdeaConfigurableUi<DebuggerSettings>
-{
-	private JCheckBox myCbStepInfoFiltersEnabled;
-	private JCheckBox myCbSkipSyntheticMethods;
-	private JCheckBox myCbSkipConstructors;
-	private JCheckBox myCbSkipClassLoaders;
-	private ClassFilterEditor mySteppingFilterEditor;
-	private JCheckBox myCbSkipSimpleGetters;
+class DebuggerSteppingConfigurable implements IdeaConfigurableUi<DebuggerSettings> {
+    private JCheckBox myCbStepInfoFiltersEnabled;
+    private JCheckBox myCbSkipSyntheticMethods;
+    private JCheckBox myCbSkipConstructors;
+    private JCheckBox myCbSkipClassLoaders;
+    private ClassFilterEditor mySteppingFilterEditor;
+    private JCheckBox myCbSkipSimpleGetters;
 
-	@Override
-	public void reset(@Nonnull DebuggerSettings settings)
-	{
-		myCbSkipSimpleGetters.setSelected(settings.SKIP_GETTERS);
-		myCbSkipSyntheticMethods.setSelected(settings.SKIP_SYNTHETIC_METHODS);
-		myCbSkipConstructors.setSelected(settings.SKIP_CONSTRUCTORS);
-		myCbSkipClassLoaders.setSelected(settings.SKIP_CLASSLOADERS);
+    @Override
+    public void reset(@Nonnull DebuggerSettings settings) {
+        myCbSkipSimpleGetters.setSelected(settings.SKIP_GETTERS);
+        myCbSkipSyntheticMethods.setSelected(settings.SKIP_SYNTHETIC_METHODS);
+        myCbSkipConstructors.setSelected(settings.SKIP_CONSTRUCTORS);
+        myCbSkipClassLoaders.setSelected(settings.SKIP_CLASSLOADERS);
 
-		myCbStepInfoFiltersEnabled.setSelected(settings.TRACING_FILTERS_ENABLED);
+        myCbStepInfoFiltersEnabled.setSelected(settings.TRACING_FILTERS_ENABLED);
 
-		mySteppingFilterEditor.setFilters(settings.getSteppingFilters());
-		mySteppingFilterEditor.setEnabled(settings.TRACING_FILTERS_ENABLED);
-	}
+        mySteppingFilterEditor.setFilters(settings.getSteppingFilters());
+        mySteppingFilterEditor.setEnabled(settings.TRACING_FILTERS_ENABLED);
+    }
 
-	@Override
-	public void apply(@Nonnull DebuggerSettings settings)
-	{
-		getSettingsTo(settings);
-	}
+    @Override
+    public void apply(@Nonnull DebuggerSettings settings) {
+        getSettingsTo(settings);
+    }
 
-	private void getSettingsTo(DebuggerSettings settings)
-	{
-		settings.SKIP_GETTERS = myCbSkipSimpleGetters.isSelected();
-		settings.SKIP_SYNTHETIC_METHODS = myCbSkipSyntheticMethods.isSelected();
-		settings.SKIP_CONSTRUCTORS = myCbSkipConstructors.isSelected();
-		settings.SKIP_CLASSLOADERS = myCbSkipClassLoaders.isSelected();
-		settings.TRACING_FILTERS_ENABLED = myCbStepInfoFiltersEnabled.isSelected();
+    private void getSettingsTo(DebuggerSettings settings) {
+        settings.SKIP_GETTERS = myCbSkipSimpleGetters.isSelected();
+        settings.SKIP_SYNTHETIC_METHODS = myCbSkipSyntheticMethods.isSelected();
+        settings.SKIP_CONSTRUCTORS = myCbSkipConstructors.isSelected();
+        settings.SKIP_CLASSLOADERS = myCbSkipClassLoaders.isSelected();
+        settings.TRACING_FILTERS_ENABLED = myCbStepInfoFiltersEnabled.isSelected();
 
-		mySteppingFilterEditor.stopEditing();
-		settings.setSteppingFilters(mySteppingFilterEditor.getFilters());
-	}
+        mySteppingFilterEditor.stopEditing();
+        settings.setSteppingFilters(mySteppingFilterEditor.getFilters());
+    }
 
-	@Override
-	public boolean isModified(@Nonnull DebuggerSettings currentSettings)
-	{
-		DebuggerSettings debuggerSettings = currentSettings.clone();
-		getSettingsTo(debuggerSettings);
-		return !debuggerSettings.equals(currentSettings);
-	}
+    @Override
+    public boolean isModified(@Nonnull DebuggerSettings currentSettings) {
+        DebuggerSettings debuggerSettings = currentSettings.clone();
+        getSettingsTo(debuggerSettings);
+        return !debuggerSettings.equals(currentSettings);
+    }
 
-	@Override
-	@Nonnull
-	public JComponent getComponent()
-	{
-		final JPanel panel = new JPanel(new GridBagLayout());
-		myCbSkipSyntheticMethods = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.synthetic.methods"));
-		myCbSkipConstructors = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.constructors"));
-		myCbSkipClassLoaders = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.classLoaders"));
-		myCbSkipSimpleGetters = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.simple.getters"));
-		myCbStepInfoFiltersEnabled = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.step.filters.list.header"));
-		panel.add(myCbSkipSyntheticMethods, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-		panel.add(myCbSkipConstructors, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-		panel.add(myCbSkipClassLoaders, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-		panel.add(myCbSkipSimpleGetters, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-		panel.add(myCbStepInfoFiltersEnabled, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(8, 0, 0, 0), 0, 0));
+    @Override
+    @Nonnull
+    public JComponent getComponent(@Nonnull Disposable parentDisposable) {
+        final JPanel panel = new JPanel(new GridBagLayout());
+        myCbSkipSyntheticMethods = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.synthetic.methods"));
+        myCbSkipConstructors = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.constructors"));
+        myCbSkipClassLoaders = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.classLoaders"));
+        myCbSkipSimpleGetters = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.skip.simple.getters"));
+        myCbStepInfoFiltersEnabled = new JCheckBox(DebuggerBundle.message("label.debugger.general.configurable.step.filters.list.header"));
+        panel.add(myCbSkipSyntheticMethods, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+            GridBagConstraints.NONE, JBUI.emptyInsets(), 0, 0));
+        panel.add(myCbSkipConstructors, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+            GridBagConstraints.NONE, JBUI.emptyInsets(), 0, 0));
+        panel.add(myCbSkipClassLoaders, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+            GridBagConstraints.NONE, JBUI.emptyInsets(), 0, 0));
+        panel.add(myCbSkipSimpleGetters, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+            GridBagConstraints.NONE, JBUI.emptyInsets(), 0, 0));
+        panel.add(myCbStepInfoFiltersEnabled, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+            GridBagConstraints.NONE, JBUI.insetsTop(8), 0, 0));
 
-		mySteppingFilterEditor = new ClassFilterEditor(JavaDebuggerSupport.getContextProjectForEditorFieldsInDebuggerConfigurables(), null,
-				"reference.viewBreakpoints.classFilters.newPattern");
-		panel.add(mySteppingFilterEditor, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
-				GridBagConstraints.BOTH, new Insets(0, 5, 0, 0), 0, 0));
+        mySteppingFilterEditor = new ClassFilterEditor(JavaDebuggerSupport.getContextProjectForEditorFieldsInDebuggerConfigurables(), null,
+            "reference.viewBreakpoints.classFilters.newPattern");
+        panel.add(mySteppingFilterEditor, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+            GridBagConstraints.BOTH, JBUI.insetsLeft(5), 0, 0));
 
-		myCbStepInfoFiltersEnabled.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				mySteppingFilterEditor.setEnabled(myCbStepInfoFiltersEnabled.isSelected());
-			}
-		});
-		return panel;
-	}
+        myCbStepInfoFiltersEnabled.addActionListener(e -> mySteppingFilterEditor.setEnabled(myCbStepInfoFiltersEnabled.isSelected()));
+        return panel;
+    }
 }
--- java-debugger-impl/src/main/java/com/intellij/java/debugger/impl/settings/UserRenderersConfigurable.java
+++ java-debugger-impl/src/main/java/com/intellij/java/debugger/impl/settings/UserRenderersConfigurable.java
@@ -25,306 +25,246 @@ import consulo.ui.ex.action.ActionToolbarPosition;
 import consulo.ui.ex.action.AnActionEvent;
 import consulo.ui.ex.awt.*;
 import consulo.ui.ex.awt.event.DocumentAdapter;
-
 import jakarta.annotation.Nonnull;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public final class UserRenderersConfigurable extends JPanel implements IdeaConfigurableUi<NodeRendererSettings>, Disposable
-{
-	private final JPanel myNameFieldPanel;
-	private final JTextField myNameField;
-	private final ElementsChooser<NodeRenderer> myRendererChooser;
-	private NodeRenderer myCurrentRenderer = null;
-	private final CompoundRendererConfigurable myRendererDataConfigurable = new CompoundRendererConfigurable(this);
+public final class UserRenderersConfigurable extends JPanel implements IdeaConfigurableUi<NodeRendererSettings>, Disposable {
+    private final JPanel myNameFieldPanel;
+    private final JTextField myNameField;
+    private final ElementsChooser<NodeRenderer> myRendererChooser;
+    private NodeRenderer myCurrentRenderer = null;
+    private final CompoundRendererConfigurable myRendererDataConfigurable = new CompoundRendererConfigurable(this);
 
-	public UserRenderersConfigurable()
-	{
-		super(new BorderLayout(JBUI.scale(4), 0));
+    public UserRenderersConfigurable() {
+        super(new BorderLayout(JBUI.scale(4), 0));
 
-		myRendererChooser = new ElementsChooser<NodeRenderer>(true);
-		setupRenderersList();
+        myRendererChooser = new ElementsChooser<>(true);
+        setupRenderersList();
 
-		ToolbarDecorator decorator = ToolbarDecorator.createDecorator((JTable) myRendererChooser.getComponent());
-		decorator.setToolbarPosition(ActionToolbarPosition.TOP);
-		decorator.setAddAction(new AddAction());
-		decorator.setRemoveAction(new RemoveAction());
-		decorator.setMoveUpAction(new MoveAction(true));
-		decorator.setMoveDownAction(new MoveAction(false));
-		decorator.addExtraAction(new CopyAction());
+        ToolbarDecorator decorator = ToolbarDecorator.createDecorator((JTable) myRendererChooser.getComponent());
+        decorator.setToolbarPosition(ActionToolbarPosition.TOP);
+        decorator.setAddAction(new AddAction());
+        decorator.setRemoveAction(new RemoveAction());
+        decorator.setMoveUpAction(new MoveAction(true));
+        decorator.setMoveDownAction(new MoveAction(false));
+        decorator.addExtraAction(new CopyAction());
 
-		myNameField = new JTextField();
-		myNameFieldPanel = new JPanel(new BorderLayout());
-		myNameFieldPanel.add(new JLabel(DebuggerBundle.message("label.user.renderers.configurable.renderer.name")), BorderLayout.WEST);
-		myNameFieldPanel.add(myNameField, BorderLayout.CENTER);
-		myNameFieldPanel.setVisible(false);
+        myNameField = new JTextField();
+        myNameFieldPanel = new JPanel(new BorderLayout());
+        myNameFieldPanel.add(new JLabel(DebuggerBundle.message("label.user.renderers.configurable.renderer.name")), BorderLayout.WEST);
+        myNameFieldPanel.add(myNameField, BorderLayout.CENTER);
+        myNameFieldPanel.setVisible(false);
 
-		final JPanel center = new JPanel(new BorderLayout(0, 4));
-		center.add(myNameFieldPanel, BorderLayout.NORTH);
-		center.add(myRendererDataConfigurable, BorderLayout.CENTER);
+        final JPanel center = new JPanel(new BorderLayout(0, 4));
+        center.add(myNameFieldPanel, BorderLayout.NORTH);
+        center.add(myRendererDataConfigurable, BorderLayout.CENTER);
 
-		myNameField.getDocument().addDocumentListener(new DocumentAdapter()
-		{
-			@Override
-			protected void textChanged(DocumentEvent e)
-			{
-				if(myCurrentRenderer != null)
-				{
-					myCurrentRenderer.setName(myNameField.getText());
-					myRendererChooser.refresh(myCurrentRenderer);
-				}
-			}
-		});
+        myNameField.getDocument().addDocumentListener(new DocumentAdapter() {
+            @Override
+            protected void textChanged(DocumentEvent e) {
+                if (myCurrentRenderer != null) {
+                    myCurrentRenderer.setName(myNameField.getText());
+                    myRendererChooser.refresh(myCurrentRenderer);
+                }
+            }
+        });
 
-		Splitter splitter = new Splitter(false);
-		splitter.setProportion(0.3f);
-		splitter.setFirstComponent(decorator.createPanel());
-		splitter.setSecondComponent(center);
-		add(splitter, BorderLayout.CENTER);
-	}
+        Splitter splitter = new Splitter(false);
+        splitter.setProportion(0.3f);
+        splitter.setFirstComponent(decorator.createPanel());
+        splitter.setSecondComponent(center);
+        add(splitter, BorderLayout.CENTER);
+    }
 
-	@Override
-	public void dispose()
-	{
-	}
+    @Override
+    public void dispose() {
+    }
 
-	@Override
-	@Nonnull
-	public JComponent getComponent()
-	{
-		return this;
-	}
+    @Override
+    @Nonnull
+    public JComponent getComponent(@Nonnull Disposable parentDisposable) {
+        return this;
+    }
 
-	private void setupRenderersList()
-	{
-		myRendererChooser.getEmptyText().setText(DebuggerBundle.message("text.user.renderers.configurable.no.renderers"));
+    private void setupRenderersList() {
+        myRendererChooser.getEmptyText().setText(DebuggerBundle.message("text.user.renderers.configurable.no.renderers"));
 
-		myRendererChooser.addElementsMarkListener(new ElementsChooser.ElementsMarkListener<NodeRenderer>()
-		{
-			@Override
-			public void elementMarkChanged(final NodeRenderer element, final boolean isMarked)
-			{
-				element.setEnabled(isMarked);
-			}
-		});
-		myRendererChooser.addListSelectionListener(new ListSelectionListener()
-		{
-			@Override
-			public void valueChanged(@Nonnull ListSelectionEvent e)
-			{
-				if(!e.getValueIsAdjusting())
-				{
-					updateCurrentRenderer(myRendererChooser.getSelectedElements());
-				}
-			}
-		});
-	}
+        myRendererChooser.addElementsMarkListener((ElementsChooser.ElementsMarkListener<NodeRenderer>) NodeRenderer::setEnabled);
+        myRendererChooser.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                updateCurrentRenderer(myRendererChooser.getSelectedElements());
+            }
+        });
+    }
 
-	private void updateCurrentRenderer(List<NodeRenderer> selectedElements)
-	{
-		if(selectedElements.size() != 1)
-		{
-			// multi selection
-			setCurrentRenderer(null);
-		}
-		else
-		{
-			setCurrentRenderer(selectedElements.get(0));
-		}
-	}
+    private void updateCurrentRenderer(List<NodeRenderer> selectedElements) {
+        if (selectedElements.size() != 1) {
+            // multi selection
+            setCurrentRenderer(null);
+        }
+        else {
+            setCurrentRenderer(selectedElements.get(0));
+        }
+    }
 
-	private void setCurrentRenderer(NodeRenderer renderer)
-	{
-		if(myCurrentRenderer == renderer)
-		{
-			return;
-		}
-		if(myRendererDataConfigurable.isModified())
-		{
-			myRendererDataConfigurable.apply();
-		}
-		myCurrentRenderer = renderer;
-		if(renderer != null)
-		{
-			myNameFieldPanel.setVisible(true);
-			myNameField.setText(renderer.getName());
-		}
-		else
-		{
-			myNameFieldPanel.setVisible(false);
-			myNameField.setText("");
-		}
-		myRendererDataConfigurable.setRenderer(renderer);
-	}
+    private void setCurrentRenderer(NodeRenderer renderer) {
+        if (myCurrentRenderer == renderer) {
+            return;
+        }
+        if (myRendererDataConfigurable.isModified()) {
+            myRendererDataConfigurable.apply();
+        }
+        myCurrentRenderer = renderer;
+        if (renderer != null) {
+            myNameFieldPanel.setVisible(true);
+            myNameField.setText(renderer.getName());
+        }
+        else {
+            myNameFieldPanel.setVisible(false);
+            myNameField.setText("");
+        }
+        myRendererDataConfigurable.setRenderer(renderer);
+    }
 
-	@Override
-	public void apply(@Nonnull NodeRendererSettings settings)
-	{
-		myRendererDataConfigurable.apply();
-		flushTo(settings.getCustomRenderers());
+    @Override
+    public void apply(@Nonnull NodeRendererSettings settings) {
+        myRendererDataConfigurable.apply();
+        flushTo(settings.getCustomRenderers());
 
-		settings.fireRenderersChanged();
-	}
+        settings.fireRenderersChanged();
+    }
 
-	private void flushTo(final RendererConfiguration rendererConfiguration)
-	{
-		final int count = myRendererChooser.getElementCount();
-		final List<NodeRenderer> renderers = new ArrayList<NodeRenderer>(count);
-		for(int idx = 0; idx < count; idx++)
-		{
-			renderers.add(myRendererChooser.getElementAt(idx));
-		}
-		rendererConfiguration.setRenderers(renderers);
-	}
+    private void flushTo(final RendererConfiguration rendererConfiguration) {
+        final int count = myRendererChooser.getElementCount();
+        final List<NodeRenderer> renderers = new ArrayList<NodeRenderer>(count);
+        for (int idx = 0; idx < count; idx++) {
+            renderers.add(myRendererChooser.getElementAt(idx));
+        }
+        rendererConfiguration.setRenderers(renderers);
+    }
 
-	@Override
-	public boolean isModified(@Nonnull NodeRendererSettings settings)
-	{
-		if(myRendererDataConfigurable.isModified())
-		{
-			return true;
-		}
-		final RendererConfiguration rendererConfiguration = settings.getCustomRenderers();
-		if(myRendererChooser.getElementCount() != rendererConfiguration.getRendererCount())
-		{
-			return true;
-		}
-		final RendererConfiguration uiConfiguration = new RendererConfiguration(settings);
-		flushTo(uiConfiguration);
-		return !uiConfiguration.equals(rendererConfiguration);
-	}
+    @Override
+    public boolean isModified(@Nonnull NodeRendererSettings settings) {
+        if (myRendererDataConfigurable.isModified()) {
+            return true;
+        }
+        final RendererConfiguration rendererConfiguration = settings.getCustomRenderers();
+        if (myRendererChooser.getElementCount() != rendererConfiguration.getRendererCount()) {
+            return true;
+        }
+        final RendererConfiguration uiConfiguration = new RendererConfiguration(settings);
+        flushTo(uiConfiguration);
+        return !uiConfiguration.equals(rendererConfiguration);
+    }
 
-	@Override
-	public void reset(@Nonnull NodeRendererSettings settings)
-	{
-		myRendererChooser.removeAllElements();
-		final RendererConfiguration rendererConfiguration = settings.getCustomRenderers();
-		final ArrayList<NodeRenderer> elementsToSelect = new ArrayList<NodeRenderer>(1);
-		rendererConfiguration.iterateRenderers(new Predicate<NodeRenderer>()
-		{
-			@Override
-			public boolean test(final NodeRenderer renderer)
-			{
-				final NodeRenderer clonedRenderer = (NodeRenderer) renderer.clone();
-				myRendererChooser.addElement(clonedRenderer, clonedRenderer.isEnabled());
-				if(elementsToSelect.size() == 0)
-				{
-					elementsToSelect.add(clonedRenderer);
-				}
-				return true;
-			}
-		});
-		myRendererChooser.selectElements(elementsToSelect);
-		updateCurrentRenderer(elementsToSelect);
-		myRendererDataConfigurable.reset();
-	}
+    @Override
+    public void reset(@Nonnull NodeRendererSettings settings) {
+        myRendererChooser.removeAllElements();
+        final RendererConfiguration rendererConfiguration = settings.getCustomRenderers();
+        final ArrayList<NodeRenderer> elementsToSelect = new ArrayList<NodeRenderer>(1);
+        rendererConfiguration.iterateRenderers(new Predicate<NodeRenderer>() {
+            @Override
+            public boolean test(final NodeRenderer renderer) {
+                final NodeRenderer clonedRenderer = (NodeRenderer) renderer.clone();
+                myRendererChooser.addElement(clonedRenderer, clonedRenderer.isEnabled());
+                if (elementsToSelect.size() == 0) {
+                    elementsToSelect.add(clonedRenderer);
+                }
+                return true;
+            }
+        });
+        myRendererChooser.selectElements(elementsToSelect);
+        updateCurrentRenderer(elementsToSelect);
+        myRendererDataConfigurable.reset();
+    }
 
-	public void addRenderer(NodeRenderer renderer)
-	{
-		myRendererChooser.addElement(renderer, renderer.isEnabled());
-	}
+    public void addRenderer(NodeRenderer renderer) {
+        myRendererChooser.addElement(renderer, renderer.isEnabled());
+    }
 
-	private class AddAction implements AnActionButtonRunnable
-	{
-		//public AddAction() {
-		//  super(DebuggerBundle.message("button.add"), DebuggerBundle.message("user.renderers.configurable.button.description.add"), ADD_ICON);
-		//}
+    private class AddAction implements AnActionButtonRunnable {
+        //public AddAction() {
+        //  super(DebuggerBundle.message("button.add"), DebuggerBundle.message("user.renderers.configurable.button.description.add"), ADD_ICON);
+        //}
 
-		@Override
-		public void run(AnActionButton button)
-		{
-			final NodeRenderer renderer = (NodeRenderer) NodeRendererSettings.getInstance().createRenderer(CompoundNodeRenderer.UNIQUE_ID);
-			renderer.setEnabled(true);
-			addRenderer(renderer);
-			SwingUtilities.invokeLater(new Runnable()
-			{
-				@Override
-				public void run()
-				{
-					myNameField.requestFocus();
-				}
-			});
-		}
-	}
+        @Override
+        public void run(AnActionButton button) {
+            final NodeRenderer renderer = (NodeRenderer) NodeRendererSettings.getInstance().createRenderer(CompoundNodeRenderer.UNIQUE_ID);
+            renderer.setEnabled(true);
+            addRenderer(renderer);
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    myNameField.requestFocus();
+                }
+            });
+        }
+    }
 
-	private class RemoveAction implements AnActionButtonRunnable
-	{
-		//public RemoveAction() {
-		//  super(DebuggerBundle.message("button.remove"), DebuggerBundle.message("user.renderers.configurable.button.description.remove"), REMOVE_ICON);
-		//}
+    private class RemoveAction implements AnActionButtonRunnable {
+        //public RemoveAction() {
+        //  super(DebuggerBundle.message("button.remove"), DebuggerBundle.message("user.renderers.configurable.button.description.remove"), REMOVE_ICON);
+        //}
 
 
-		@Override
-		public void run(AnActionButton button)
-		{
-			for(NodeRenderer selectedElement : myRendererChooser.getSelectedElements())
-			{
-				myRendererChooser.removeElement(selectedElement);
-			}
-		}
-	}
+        @Override
+        public void run(AnActionButton button) {
+            for (NodeRenderer selectedElement : myRendererChooser.getSelectedElements()) {
+                myRendererChooser.removeElement(selectedElement);
+            }
+        }
+    }
 
-	private class CopyAction extends AnActionButton
-	{
-		public CopyAction()
-		{
-			super(DebuggerBundle.message("button.copy"), DebuggerBundle.message("user.renderers.configurable.button.description.copy"), AllIcons.Actions.Copy);
-		}
+    private class CopyAction extends AnActionButton {
+        public CopyAction() {
+            super(DebuggerBundle.message("button.copy"), DebuggerBundle.message("user.renderers.configurable.button.description.copy"), AllIcons.Actions.Copy);
+        }
 
-		@Override
-		public void actionPerformed(AnActionEvent e)
-		{
-			final NodeRenderer selectedElement = myRendererChooser.getSelectedElement();
-			if(selectedElement != null)
-			{
-				myRendererChooser.addElement((NodeRenderer) selectedElement.clone(), true);
-			}
-		}
+        @Override
+        public void actionPerformed(AnActionEvent e) {
+            final NodeRenderer selectedElement = myRendererChooser.getSelectedElement();
+            if (selectedElement != null) {
+                myRendererChooser.addElement((NodeRenderer) selectedElement.clone(), true);
+            }
+        }
 
-		@Override
-		public void updateButton(AnActionEvent e)
-		{
-			super.updateButton(e);
-			e.getPresentation().setEnabled(myRendererChooser.getSelectedElement() != null);
-		}
-	}
+        @Override
+        public void updateButton(AnActionEvent e) {
+            super.updateButton(e);
+            e.getPresentation().setEnabled(myRendererChooser.getSelectedElement() != null);
+        }
+    }
 
-	private class MoveAction implements AnActionButtonRunnable
-	{
-		private final boolean myMoveUp;
+    private class MoveAction implements AnActionButtonRunnable {
+        private final boolean myMoveUp;
 
-		public MoveAction(boolean up)
-		{
-			//super(up? DebuggerBundle.message("button.move.up") : DebuggerBundle.message("button.move.down"),
-			//      up? DebuggerBundle.message("user.renderers.configurable.button.description.move.up") : DebuggerBundle.message("user.renderers.configurable.button.description.move.down"),
-			//      up? UP_ICON : DOWN_ICON );
-			myMoveUp = up;
-		}
+        public MoveAction(boolean up) {
+            //super(up? DebuggerBundle.message("button.move.up") : DebuggerBundle.message("button.move.down"),
+            //      up? DebuggerBundle.message("user.renderers.configurable.button.description.move.up") : DebuggerBundle.message("user.renderers.configurable.button.description.move.down"),
+            //      up? UP_ICON : DOWN_ICON );
+            myMoveUp = up;
+        }
 
-		@Override
-		public void run(AnActionButton button)
-		{
-			final int selectedRow = myRendererChooser.getSelectedElementRow();
-			if(selectedRow < 0)
-			{
-				return;
-			}
-			int newRow = selectedRow + (myMoveUp ? -1 : 1);
-			if(newRow < 0)
-			{
-				newRow = myRendererChooser.getElementCount() - 1;
-			}
-			else if(newRow >= myRendererChooser.getElementCount())
-			{
-				newRow = 0;
-			}
-			myRendererChooser.moveElement(myRendererChooser.getElementAt(selectedRow), newRow);
-		}
-	}
+        @Override
+        public void run(AnActionButton button) {
+            final int selectedRow = myRendererChooser.getSelectedElementRow();
+            if (selectedRow < 0) {
+                return;
+            }
+            int newRow = selectedRow + (myMoveUp ? -1 : 1);
+            if (newRow < 0) {
+                newRow = myRendererChooser.getElementCount() - 1;
+            }
+            else if (newRow >= myRendererChooser.getElementCount()) {
+                newRow = 0;
+            }
+            myRendererChooser.moveElement(myRendererChooser.getElementAt(selectedRow), newRow);
+        }
+    }
 }
