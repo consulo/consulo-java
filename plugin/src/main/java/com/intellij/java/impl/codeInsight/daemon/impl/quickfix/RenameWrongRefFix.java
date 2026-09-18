eb35b111b3864f80df2071b5fc613c7c9c54946d
UNV <nikolay@yurchenko.su>
2026-09-19 01:50:53 +0300

    Replacing consulo.ide.impl.idea.openapi.editor.ex.util.EditorUtil with AWTEditorUtil.

--- plugin/src/main/java/com/intellij/java/impl/codeInsight/daemon/impl/quickfix/RenameWrongRefFix.java
+++ plugin/src/main/java/com/intellij/java/impl/codeInsight/daemon/impl/quickfix/RenameWrongRefFix.java
@@ -13,22 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Created by IntelliJ IDEA.
- * User: mike
- * Date: Aug 20, 2002
- * Time: 8:49:24 PM
- * To change template for new class use
- * Code Style | Class Templates options (Tools | IDE Options).
- */
 package com.intellij.java.impl.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.java.language.psi.*;
+import consulo.annotation.access.RequiredReadAction;
 import consulo.codeEditor.Editor;
+import consulo.codeEditor.util.AWTEditorUtil;
 import consulo.document.util.TextRange;
-import consulo.ide.impl.idea.openapi.editor.ex.util.EditorUtil;
-import consulo.java.analysis.impl.JavaQuickFixBundle;
 import consulo.java.analysis.impl.localize.JavaQuickFixLocalize;
 import consulo.language.editor.FileModificationService;
 import consulo.language.editor.completion.lookup.LookupElement;
@@ -44,183 +35,206 @@ import consulo.language.psi.resolve.ResolveState;
 import consulo.language.psi.util.PsiTreeUtil;
 import consulo.localize.LocalizeValue;
 import consulo.project.Project;
+import consulo.ui.annotation.RequiredUIAccess;
 
 import java.util.*;
 
+/**
+ * @author mike
+ * @since 2002-08-20
+ */
 public class RenameWrongRefFix implements SyntheticIntentionAction {
-  private final PsiReferenceExpression myRefExpr;
-  private static final String INPUT_VARIABLE_NAME = "INPUTVAR";
-  private static final String OTHER_VARIABLE_NAME = "OTHERVAR";
-  private final boolean myUnresolvedOnly;
+    private final PsiReferenceExpression myRefExpr;
+    private static final String INPUT_VARIABLE_NAME = "INPUTVAR";
+    private static final String OTHER_VARIABLE_NAME = "OTHERVAR";
+    private final boolean myUnresolvedOnly;
 
-  public RenameWrongRefFix(PsiReferenceExpression refExpr) {
-    this(refExpr, false);
-  }
-
-  public RenameWrongRefFix(PsiReferenceExpression refExpr, boolean unresolvedOnly) {
-    myRefExpr = refExpr;
-    myUnresolvedOnly = unresolvedOnly;
-  }
-
-  @Override
-  public LocalizeValue getText() {
-    return JavaQuickFixLocalize.renameWrongReferenceText();
-  }
-
-  @Override
-  public boolean isAvailable(Project project, Editor editor, PsiFile file) {
-    if (!myRefExpr.isValid() || !myRefExpr.getManager().isInProject(myRefExpr)) return false;
-    int offset = editor.getCaretModel().getOffset();
-    PsiElement refName = myRefExpr.getReferenceNameElement();
-    if (refName == null) return false;
-    TextRange textRange = refName.getTextRange();
-    if (textRange == null || offset < textRange.getStartOffset() ||
-        offset > textRange.getEndOffset()) {
-      return false;
+    public RenameWrongRefFix(PsiReferenceExpression refExpr) {
+        this(refExpr, false);
     }
 
-    return !CreateFromUsageUtils.isValidReference(myRefExpr, myUnresolvedOnly);
-  }
+    public RenameWrongRefFix(PsiReferenceExpression refExpr, boolean unresolvedOnly) {
+        myRefExpr = refExpr;
+        myUnresolvedOnly = unresolvedOnly;
+    }
 
-  private class ReferenceNameExpression extends Expression {
-    class HammingComparator implements Comparator<LookupElement> {
-      @Override
-      public int compare(LookupElement lookupItem1, LookupElement lookupItem2) {
-        String s1 = lookupItem1.getLookupString();
-        String s2 = lookupItem2.getLookupString();
-        int diff1 = 0;
-        for (int i = 0; i < Math.min(s1.length(), myOldReferenceName.length()); i++) {
-          if (s1.charAt(i) != myOldReferenceName.charAt(i)) diff1++;
+    @Override
+    public LocalizeValue getText() {
+        return JavaQuickFixLocalize.renameWrongReferenceText();
+    }
+
+    @Override
+    @RequiredReadAction
+    public boolean isAvailable(Project project, Editor editor, PsiFile file) {
+        if (!myRefExpr.isValid() || !myRefExpr.getManager().isInProject(myRefExpr)) {
+            return false;
         }
-        int diff2 = 0;
-        for (int i = 0; i < Math.min(s2.length(), myOldReferenceName.length()); i++) {
-          if (s2.charAt(i) != myOldReferenceName.charAt(i)) diff2++;
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement refName = myRefExpr.getReferenceNameElement();
+        if (refName == null) {
+            return false;
         }
-        return diff1 - diff2;
-      }
+        TextRange textRange = refName.getTextRange();
+        if (textRange == null || offset < textRange.getStartOffset() ||
+            offset > textRange.getEndOffset()) {
+            return false;
+        }
+
+        return !CreateFromUsageUtils.isValidReference(myRefExpr, myUnresolvedOnly);
     }
 
-    ReferenceNameExpression(LookupElement[] items, String oldReferenceName) {
-      myItems = items;
-      myOldReferenceName = oldReferenceName;
-      Arrays.sort(myItems, new HammingComparator ());
-    }
+    private class ReferenceNameExpression extends Expression {
+        class HammingComparator implements Comparator<LookupElement> {
+            @Override
+            public int compare(LookupElement lookupItem1, LookupElement lookupItem2) {
+                String s1 = lookupItem1.getLookupString();
+                String s2 = lookupItem2.getLookupString();
+                int diff1 = 0;
+                for (int i = 0; i < Math.min(s1.length(), myOldReferenceName.length()); i++) {
+                    if (s1.charAt(i) != myOldReferenceName.charAt(i)) {
+                        diff1++;
+                    }
+                }
+                int diff2 = 0;
+                for (int i = 0; i < Math.min(s2.length(), myOldReferenceName.length()); i++) {
+                    if (s2.charAt(i) != myOldReferenceName.charAt(i)) {
+                        diff2++;
+                    }
+                }
+                return diff1 - diff2;
+            }
+        }
 
-    LookupElement[] myItems;
-    private final String myOldReferenceName;
+        ReferenceNameExpression(LookupElement[] items, String oldReferenceName) {
+            myItems = items;
+            myOldReferenceName = oldReferenceName;
+            Arrays.sort(myItems, new HammingComparator());
+        }
 
-    @Override
-    public Result calculateResult(ExpressionContext context) {
-      if (myItems == null || myItems.length == 0) {
-        return new TextResult(myOldReferenceName);
-      }
-      return new TextResult(myItems[0].getLookupString());
-    }
+        LookupElement[] myItems;
+        private final String myOldReferenceName;
 
-    @Override
-    public Result calculateQuickResult(ExpressionContext context) {
-      return null;
-    }
-
-    @Override
-    public LookupElement[] calculateLookupItems(ExpressionContext context) {
-      if (myItems == null || myItems.length == 1) return null;
-      return myItems;
-    }
-  }
-
-  private LookupElement[] collectItems() {
-    Set<LookupElement> items = new LinkedHashSet<LookupElement>();
-    boolean qualified = myRefExpr.getQualifierExpression() != null;
-
-    if (!qualified && !(myRefExpr.getParent() instanceof PsiMethodCallExpression)) {
-      PsiVariable[] vars = CreateFromUsageUtils.guessMatchingVariables(myRefExpr);
-      for (PsiVariable var : vars) {
-        items.add(LookupElementBuilder.create(var.getName()));
-      }
-    } else {
-      class MyScopeProcessor extends BaseScopeProcessor {
-        ArrayList<PsiElement> myResult = new ArrayList<PsiElement>();
-        boolean myFilterMethods;
-        boolean myFilterStatics = false;
-
-        MyScopeProcessor(PsiReferenceExpression refExpression) {
-          myFilterMethods = refExpression.getParent() instanceof PsiMethodCallExpression;
-          PsiExpression qualifier = refExpression.getQualifierExpression();
-          if (qualifier instanceof PsiReferenceExpression) {
-            PsiElement e = ((PsiReferenceExpression) qualifier).resolve();
-            myFilterStatics = e instanceof PsiClass;
-          } else if (qualifier == null) {
-            PsiModifierListOwner scope = PsiTreeUtil.getParentOfType(refExpression, PsiModifierListOwner.class);
-            myFilterStatics = scope != null && scope.hasModifierProperty(PsiModifier.STATIC);
-          }
+        @Override
+        public Result calculateResult(ExpressionContext context) {
+            if (myItems == null || myItems.length == 0) {
+                return new TextResult(myOldReferenceName);
+            }
+            return new TextResult(myItems[0].getLookupString());
         }
 
         @Override
-        public boolean execute(PsiElement element, ResolveState state) {
-          if (element instanceof PsiNamedElement
-              && element instanceof PsiModifierListOwner
-              && myFilterMethods == element instanceof PsiMethod) {
-            if (((PsiModifierListOwner)element).hasModifierProperty(PsiModifier.STATIC) == myFilterStatics) {
-              myResult.add(element);
+        public Result calculateQuickResult(ExpressionContext context) {
+            return null;
+        }
+
+        @Override
+        public LookupElement[] calculateLookupItems(ExpressionContext context) {
+            if (myItems == null || myItems.length == 1) {
+                return null;
             }
-          }
-          return true;
+            return myItems;
+        }
+    }
+
+    @RequiredReadAction
+    private LookupElement[] collectItems() {
+        Set<LookupElement> items = new LinkedHashSet<>();
+        boolean qualified = myRefExpr.getQualifierExpression() != null;
+
+        if (!qualified && !(myRefExpr.getParent() instanceof PsiMethodCallExpression)) {
+            PsiVariable[] vars = CreateFromUsageUtils.guessMatchingVariables(myRefExpr);
+            for (PsiVariable var : vars) {
+                items.add(LookupElementBuilder.create(var.getName()));
+            }
+        }
+        else {
+            class MyScopeProcessor extends BaseScopeProcessor {
+                List<PsiElement> myResult = new ArrayList<>();
+                boolean myFilterMethods;
+                boolean myFilterStatics = false;
+
+                @RequiredReadAction
+                MyScopeProcessor(PsiReferenceExpression refExpression) {
+                    myFilterMethods = refExpression.getParent() instanceof PsiMethodCallExpression;
+                    PsiExpression qualifier = refExpression.getQualifierExpression();
+                    if (qualifier instanceof PsiReferenceExpression ref) {
+                        PsiElement e = ref.resolve();
+                        myFilterStatics = e instanceof PsiClass;
+                    }
+                    else if (qualifier == null) {
+                        PsiModifierListOwner scope = PsiTreeUtil.getParentOfType(refExpression, PsiModifierListOwner.class);
+                        myFilterStatics = scope != null && scope.hasModifierProperty(PsiModifier.STATIC);
+                    }
+                }
+
+                @Override
+                public boolean execute(PsiElement element, ResolveState state) {
+                    if (element instanceof PsiNamedElement
+                        && element instanceof PsiModifierListOwner modifierListOwner
+                        && myFilterMethods == element instanceof PsiMethod) {
+                        if (modifierListOwner.hasModifierProperty(PsiModifier.STATIC) == myFilterStatics) {
+                            myResult.add(element);
+                        }
+                    }
+                    return true;
+                }
+
+                public PsiElement[] getVariants() {
+                    return PsiUtilCore.toPsiElementArray(myResult);
+                }
+            }
+
+            items.add(LookupElementBuilder.create(myRefExpr.getReferenceName()));
+            MyScopeProcessor processor = new MyScopeProcessor(myRefExpr);
+            myRefExpr.processVariants(processor);
+            PsiElement[] variants = processor.getVariants();
+            for (PsiElement variant : variants) {
+                items.add(LookupElementBuilder.create(((PsiNamedElement) variant).getName()));
+            }
         }
 
-        public PsiElement[] getVariants () {
-          return PsiUtilCore.toPsiElementArray(myResult);
+        return items.toArray(new LookupElement[items.size()]);
+    }
+
+    @Override
+    @RequiredUIAccess
+    public void invoke(Project project, Editor editor, PsiFile file) {
+        if (!FileModificationService.getInstance().prepareFileForWrite(file)) {
+            return;
         }
-      }
+        PsiReferenceExpression[] refs = CreateFromUsageUtils.collectExpressions(myRefExpr, PsiMember.class, PsiFile.class);
+        PsiElement element = PsiTreeUtil.getParentOfType(myRefExpr, PsiMember.class, PsiFile.class);
+        LookupElement[] items = collectItems();
+        ReferenceNameExpression refExpr = new ReferenceNameExpression(items, myRefExpr.getReferenceName());
 
-      items.add(LookupElementBuilder.create(myRefExpr.getReferenceName()));
-      MyScopeProcessor processor = new MyScopeProcessor(myRefExpr);
-      myRefExpr.processVariants(processor);
-      PsiElement[] variants = processor.getVariants();
-      for (PsiElement variant : variants) {
-        items.add(LookupElementBuilder.create(((PsiNamedElement)variant).getName()));
-      }
+        TemplateBuilder builder = TemplateBuilderFactory.getInstance().createTemplateBuilder(element);
+        for (PsiReferenceExpression expr : refs) {
+            if (!expr.equals(myRefExpr)) {
+                builder.replaceElement(expr.getReferenceNameElement(), OTHER_VARIABLE_NAME, INPUT_VARIABLE_NAME, false);
+            }
+            else {
+                builder.replaceElement(expr.getReferenceNameElement(), INPUT_VARIABLE_NAME, refExpr, true);
+            }
+        }
+
+        float proportion = AWTEditorUtil.calcVerticalScrollProportion(editor);
+        editor.getCaretModel().moveToOffset(element.getTextRange().getStartOffset());
+
+        /*for (int i = refs.length - 1; i >= 0; i--) {
+            TextRange range = refs[i].getReferenceNameElement().getTextRange();
+            document.deleteString(range.getStartOffset(), range.getEndOffset());
+        }*/
+
+        Template template = builder.buildInlineTemplate();
+        editor.getCaretModel().moveToOffset(element.getTextRange().getStartOffset());
+
+        TemplateManager.getInstance(project).startTemplate(editor, template);
+
+        AWTEditorUtil.setVerticalScrollProportion(editor, proportion);
     }
 
-    return items.toArray(new LookupElement[items.size()]);
-  }
-
-  @Override
-  public void invoke(Project project, Editor editor, PsiFile file) {
-    if (!FileModificationService.getInstance().prepareFileForWrite(file)) return;
-    PsiReferenceExpression[] refs = CreateFromUsageUtils.collectExpressions(myRefExpr, PsiMember.class, PsiFile.class);
-    PsiElement element = PsiTreeUtil.getParentOfType(myRefExpr, PsiMember.class, PsiFile.class);
-    LookupElement[] items = collectItems();
-    ReferenceNameExpression refExpr = new ReferenceNameExpression(items, myRefExpr.getReferenceName());
-
-    TemplateBuilder builder = TemplateBuilderFactory.getInstance().createTemplateBuilder(element);
-    for (PsiReferenceExpression expr : refs) {
-      if (!expr.equals(myRefExpr)) {
-        builder.replaceElement(expr.getReferenceNameElement(), OTHER_VARIABLE_NAME, INPUT_VARIABLE_NAME, false);
-      }
-      else {
-        builder.replaceElement(expr.getReferenceNameElement(), INPUT_VARIABLE_NAME, refExpr, true);
-      }
+    @Override
+    public boolean startInWriteAction() {
+        return true;
     }
-
-    float proportion = EditorUtil.calcVerticalScrollProportion(editor);
-    editor.getCaretModel().moveToOffset(element.getTextRange().getStartOffset());
-
-    /*for (int i = refs.length - 1; i >= 0; i--) {
-      TextRange range = refs[i].getReferenceNameElement().getTextRange();
-      document.deleteString(range.getStartOffset(), range.getEndOffset());
-    }
-*/
-    Template template = builder.buildInlineTemplate();
-    editor.getCaretModel().moveToOffset(element.getTextRange().getStartOffset());
-
-    TemplateManager.getInstance(project).startTemplate(editor, template);
-
-    EditorUtil.setVerticalScrollProportion(editor, proportion);
-  }
-
-  @Override
-  public boolean startInWriteAction() {
-    return true;
-  }
 }
